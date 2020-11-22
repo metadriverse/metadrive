eecaf8af581c90704e4ce9da880b0788a886daa8
PENG Zhenghao <pengzh@ie.cuhk.edu.hk>
2020-11-22 08:33:24 +0800

    Add CI to check if formated (#4)
    * add CI to check format
    
    * add install
    
    * add install

--- format.sh
+++ format.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
-# Usage: at the root dir >> bash format.sh
-yapf --in-place --recursive -p --verbose --style .style.yapf pg_drive
+# Usage: at the root dir >> bash scripts/format.sh
+
+# Check yapf version. (20200318 latest is 0.29.0. Format might be changed in future version.)
+ver=$(yapf --version)
+if ! echo $ver | grep -q 0.30.0; then
+  echo "Wrong YAPF version installed: 0.30.0 is required, not $ver."
+  exit 1
+fi
+
+yapf --in-place --recursive -p --verbose --style .style.yapf pg_drive/
+
+if [[ "$1" == '--test' ]]; then # Only for CI usage, user should not use --test flag.
+  if ! git diff --quiet &>/dev/null; then
+    echo '*** You have not formatted your code! Please run [bash format.sh] at root directory before commit! Thanks! ***'
+    exit 1
+  else
+    echo "Code style test passed!"
+  fi
+fi
