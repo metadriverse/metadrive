74c0f9242454bcfc550e008904a08bf2cd8c31e0
lqy0057 <785878978@qq.com>
2020-12-11 16:17:40 +0800

    Test install (#66)
    * install test script
    
    * fix fanche bug
    
    * format

--- pg_drive/tests/install_test/test_get_image.py
+++ pg_drive/tests/install_test/test_get_image.py
@@ -4,8 +4,8 @@ from pg_drive.envs.generalization_racing import GeneralizationRacing
 from pg_drive.scene_creator.algorithm.BIG import BigGenerateMethod
 from pg_drive.scene_creator.map import Map
 
-if __name__ == "__main__":
-    headless = False
+
+def capture_image(headless):
     env = GeneralizationRacing(
         dict(
             use_render=False,
@@ -14,17 +14,14 @@ if __name__ == "__main__":
                 Map.GENERATE_PARA: 7
             },
             traffic_density=0.5,
-            manual_control=True,
-            traffic_mode=0,
             use_image=True,
             pg_world_config=dict(headless_image=headless)
         )
     )
-
     start = time.time()
     env.reset()
     print("Render cost time: ", time.time() - start)
-    for i in range(30):
+    for i in range(3):
         o, r, d, info = env.step([0, 1])
         from panda3d.core import PNMImage
 
@@ -32,3 +29,9 @@ if __name__ == "__main__":
         env.pg_world.win.getScreenshot(img)
         img.write("{}_{}.png".format("headless" if headless else "local", i))
     env.close()
+
+
+if __name__ == "__main__":
+    headless = False
+    capture_image(False)
+    print("Offscreen render launched successfully! Images are saved, Please check them.")
