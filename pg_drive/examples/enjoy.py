da270d864e8dab5903d4434013851ee5e755210e
PENG Zhenghao <pengzh@ie.cuhk.edu.hk>
2020-12-12 00:24:59 +0800

    Update some test scripts (#68)
    * add test script for image obs
    
    * update the script
    
    * update
    
    * update
    
    * update
    
    * format
    
    * update the enjoy script

--- pg_drive/examples/enjoy.py
+++ pg_drive/examples/enjoy.py
@@ -1,5 +1,6 @@
 from pg_drive.envs.generalization_racing import GeneralizationRacing
 from pg_drive.scene_creator.algorithm.BIG import BigGenerateMethod
+from pg_drive.scene_manager.traffic_manager import TrafficMode
 from pg_drive.utils import setup_logger
 
 setup_logger(debug=True)
@@ -9,16 +10,18 @@ if __name__ == "__main__":
         dict(
             use_render=True,
             manual_control=True,
+            traffic_density=0.2,
+            traffic_mode=TrafficMode.Reborn,
+            environment_num=100,
             map_config={
                 "type": BigGenerateMethod.BLOCK_NUM,
                 "config": 7,
             },
         )
     )
+    print("Press h to see help message!")
     env.reset()
     for i in range(1, 100000):
         o, r, d, info = env.step([0, 0])
         env.render()
-        if d:
-            env.reset()
     env.close()
