e41071d1610d6280d3bca4160e464ffc82ba9f33
lqy0057 <785878978@qq.com>
2020-12-18 01:24:32 +0800

    Fix negative speed (#123)
    * script
    
    * fix finish
    
    * test script
    
    * add print
    
    * add velocity print in render
    
    * format
    
    * close #117  #116 #113 
    
    Co-authored-by: pengzhenghao <pengzh@ie.cuhk.edu.hk>

---
+++ pgdrive/tests/vehicle_test/test_nan_speed.py
@@ -0,0 +1,44 @@
+from pgdrive.envs.pgdrive_env import PGDriveEnv
+from pgdrive.scene_creator.map import Map, MapGenerateMethod
+from pgdrive.utils import setup_logger
+
+setup_logger(debug=True)
+
+
+class TestEnv(PGDriveEnv):
+    def __init__(self):
+        super(TestEnv, self).__init__(
+            {
+                "environment_num": 4,
+                "traffic_density": 0.0,
+                "use_render": True,
+                "map_config": {
+                    Map.GENERATE_METHOD: MapGenerateMethod.BIG_BLOCK_SEQUENCE,
+                    Map.GENERATE_PARA: "SSSSSSSSSSSSS",
+                }
+            }
+        )
+
+
+if __name__ == "__main__":
+    import numpy as np
+
+    env = TestEnv()
+    acc = [0, 1]
+    brake = [-1, -np.nan]
+    env.reset()
+    for i in range(1, 100000):
+        if i < 110:
+            a = acc
+        elif 110 < i < 120:
+            a = brake
+        else:
+            a = [-1, -1]
+        o, r, d, info = env.step(a)
+        env.render(
+            text="Old speed: {:.3f}, new speed: {:.3f}, diff: {:.3f}".format(
+                env.vehicle.system.get_current_speed_km_hour(), env.vehicle.speed,
+                env.vehicle.system.get_current_speed_km_hour() - env.vehicle.speed
+            )
+        )
+    env.close()
