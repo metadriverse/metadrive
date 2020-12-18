7a43c936066ffa3a4f87652bb8ec9b7deac6ff92
PENG Zhenghao <pengzh@ie.cuhk.edu.hk>
2020-12-18 23:48:58 +0800

    Update many examples (#132)
    * introduce an example notebook
    
    * update example scripts
    
    * update readme
    
    * update setup.py
    
    * add a badge
    
    Close #83

--- pgdrive/examples/enjoy.py
+++ pgdrive/examples/enjoy.py
@@ -1,5 +1,13 @@
+"""
+Please feel free to run this script to enjoy a journey by keyboard!
+Remember to press H to see help message!
+
+Note: This script require rendering, please following the installation instruction to setup a proper
+environment that allows popping up an window.
+"""
+import random
+
 from pgdrive.envs.pgdrive_env import PGDriveEnv
-from pgdrive.scene_creator.algorithm.BIG import BigGenerateMethod
 from pgdrive.scene_manager.traffic_manager import TrafficMode
 
 if __name__ == "__main__":
@@ -10,13 +18,10 @@ if __name__ == "__main__":
             traffic_density=0.2,
             traffic_mode=TrafficMode.Reborn,
             environment_num=100,
-            map_config={
-                "type": BigGenerateMethod.BLOCK_NUM,
-                "config": 7,
-            },
+            map=7,
+            start_seed=random.randint(0, 1000)
         )
     )
-    print("Press h to see help message!")
     env.reset()
     for i in range(1, 100000):
         o, r, d, info = env.step([0, 0])
