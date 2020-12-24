39e5e433065d3fecfb5547edabfd0c5aa013fb1a
PENG Zhenghao <pengzh@ie.cuhk.edu.hk>
2020-12-24 14:37:12 +0800

    Formalize the pg_world render mode (#167)
    * fix
    
    * run generate map to test
    
    * default friction to 1.0, and change friction range to (0.8, 1.2)
    
    * import pygame
    
    * format
    
    * remove a stupid assertion
    
    * optimize the pg_world.mode
    
    * format
    
    * format
    
    * format
    
    Co-authored-by: lqy0057 <785878978@qq.com>

--- pgdrive/tests/highway_render_test/test_highway_render.py
+++ pgdrive/tests/highway_render_test/test_highway_render.py
@@ -1,5 +1,4 @@
 from pgdrive.envs.pgdrive_env import PGDriveEnv
-from pgdrive.scene_creator.map import Map, MapGenerateMethod
 from pgdrive.utils import setup_logger
 
 setup_logger(True)
@@ -10,25 +9,10 @@ class TestEnv(PGDriveEnv):
         super(TestEnv, self).__init__(
             {
                 "environment_num": 1,
-                "traffic_density": 0.1,
-                "start_seed": 5,
-                "pg_world_config": {
-                    "onscreen_message": True,
-                    "highway_render": "onscreen",
-                },
-                "image_source": "mini_map",
                 "manual_control": True,
                 "use_render": False,
                 "use_image": False,
-                "steering_penalty": 0.0,
-                "decision_repeat": 5,
-                "rgb_clip": True,
-                "map_config": {
-                    Map.GENERATE_METHOD: MapGenerateMethod.BIG_BLOCK_NUM,
-                    Map.GENERATE_PARA: 12,
-                    Map.LANE_WIDTH: 3.5,
-                    Map.LANE_NUM: 3,
-                }
+                "use_topdown": True,
             }
         )
 
@@ -38,7 +22,7 @@ if __name__ == "__main__":
 
     o = env.reset()
     for i in range(1, 100000):
-        o, r, d, info = env.step([0, 1])
+        o, r, d, info = env.step([0.01, 0.1])
         env.render()
         # if d:
         #     print("Reset")
---
+++ pgdrive/world/__init__.py
@@ -0,0 +1,1 @@
+from pgdrive.world.constants import *
