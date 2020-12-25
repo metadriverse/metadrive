d4d93c45ef69054cf05ea8ca96bcde7099c2a091
lqy0057 <785878978@qq.com>
2020-12-25 12:31:14 +0800

    Controlable pid (#174)
    * finish pid auto test
    
    * format
    
    * PID can interact with human now

--- pgdrive/tests/generalization_env_test/test_navigation.py
+++ pgdrive/tests/generalization_env_test/test_navigation.py
@@ -26,24 +26,45 @@ class TestEnv(PGDriveEnv):
 
 
 if __name__ == "__main__":
-    env = TestEnv()
-    target_lateral = 0.375
-    target_speed = 30
-    o = env.reset()
 
-    steering_controller = PIDController(1.6, 0.0008, 27.1)
-    steering_error = o[0] - target_lateral
+    class Target:
+        def __init__(self, target_lateral, target_speed):
+            self.lateral = target_lateral
+            self.speed = target_speed
+
+        def go_right(self):
+            self.lateral += 0.25
+
+        def go_left(self):
+            self.lateral -= 0.25
+
+        def faster(self):
+            self.speed += 10
+
+        def slower(self):
+            self.speed -= 10
+
+    env = TestEnv()
+    target = Target(0.375, 30)
+    o = env.reset()
+    env.pg_world.accept('d', target.go_right)
+    env.pg_world.accept('a', target.go_left)
+    env.pg_world.accept('w', target.faster)
+    env.pg_world.accept('s', target.slower)
+
+    steering_controller = PIDController(1.6, 0.0008, 27.3)
+    steering_error = o[0] - target.lateral
     steering = steering_controller.get_result(steering_error)
     acc_controller = PIDController(0.1, 0.001, 0.3)
-    acc_error = env.vehicle.speed - target_speed
+    acc_error = env.vehicle.speed - target.speed
     acc = acc_controller.get_result(acc_error)
     for i in range(1, 100000):
         o, r, d, info = env.step([-steering, acc])
         # calculate new action
 
-        steering_error = o[0] - target_lateral
+        steering_error = o[0] - target.lateral
         steering = steering_controller.get_result(steering_error)
-        t_speed = target_speed if abs(o[12] - 0.5) < 0.01 else 20
+        t_speed = target.speed if abs(o[12] - 0.5) < 0.01 else target.speed - 10
         acc_error = env.vehicle.speed - t_speed
         acc = acc_controller.get_result(acc_error)
 
@@ -52,9 +73,9 @@ if __name__ == "__main__":
             print("Reset")
             o = env.reset()
             steering_controller.reset()
-            steering_error = o[0] - target_lateral
+            steering_error = o[0] - target.lateral
             steering = steering_controller.get_result(steering_error, o[11])
             acc_controller.reset()
-            acc_error = env.vehicle.speed - target_speed
+            acc_error = env.vehicle.speed - target.speed
             acc = acc_controller.get_result(acc_error)
     env.close()
