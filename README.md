420fa3bd87d61afba95dff96d9dd95e820a60628
PENG Zhenghao <pengzh@ie.cuhk.edu.hk>
2020-12-28 17:05:08 +0800

    Update website (#188)
    * update from Bolei
    
    * update images
    
    * update images
    
    * add .DS_Store to gitignore
    
    * add gif
    
    * regenerate fig
    
    * typos
    
    * refine
    
    * refine
    
    Co-authored-by: zqh0253 <3170104343@zju.edu.cn>

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Welcome to PGDrive! PGDrive is an driving simulator with many key features, incl
 - 🗺 **Open-ended**: Support generating infinite scenes and configuring various traffic, vehicle, and environmental settings.
 
 
-<img src="https://raw.githubusercontent.com/decisionforce/decisionforce.github.io/master/projects/pgdrive/panel.png">
+<img src="docs/images/panel.jpg">
 
 
 
--- docs/assets/corpus.js
+++ docs/assets/corpus.js
@@ -1,7 +1,8 @@
 var title_corpus = [
-    ['PGDrive: An Open-ended Driving Simulator <br>with Infinite Scenes', 'PGDrive：一个拥有无限场景的开放式驾驶平台'],
-    ['Overview', '总览'],
-    ['Results', '结果'],
+    ['PGDrive: An Open-ended Driving Simulator <br>with Infinite Scenes from Procedural Generation', 'PGDrive：一个拥有无限场景的开放式驾驶平台'],
+    ['Overview of PGDrive Simulator', '总览'],
+    ['Procedural Generation of Driving Scenes', '驾驶场景的过程生成'],
+    ['Result of Improved Generalization', '提升范化性的结果'],
 ]
 var author_corpus = [
     ['Quanyi Li', '黎权毅'],
@@ -12,37 +13,44 @@ var author_corpus = [
     ['Bolei Zhou', '周博磊'],
 ]
 var institution_corpus = [
-    ['<sup>1</sup>The Chinese University of Hong Kong', '<sup>1</sup>香港中文大学'],
-    ['<sup>2</sup>Sense Time Inc.', '<sup>2</sup>商汤科技'],
-    ['<sup>3</sup>Zhejiang University', '<sup>3</sup>浙江大学'],
+    ['<div><sup>1</sup>The Chinese University of Hong Kong, <sup>2</sup>SenseTime Research, <sup>3</sup>Zhejiang University</div>', 
+    '<div><sup>1</sup>香港中文大学, <sup>2</sup>商汤科技, <sup>3</sup>浙江大学</div>'],
 ]
 var text_corpus = [
-    ['To better evaluate and improve the generalization of learning-based driving system, we introduce an open-ended\
+    ['To better evaluate and improve the generalization of learning-based driving systems, we introduce an\
+    open-ended\
     and highly configurable driving simulator called PGDrive.\
-    PGDrive can generate infinite driving scenes through procedural generalization, which can benifit the research\
-    on the generalization of learning system.\
-    Please visit <a href="https://github.com/decisionforce/pgdrive">github.com/decisionforce/pgdrive</a> to enjoy\
-    PGDrive!', '为了更好地评估和改善基于强化学习的自动驾驶系统的泛化性能，我们提出了一个开放式、极易配置的模拟器：PGDrive！PGDrive可以通过过程生成技术生成无限多的地图\
+    PGDrive can generate a diverse set of driving scenes through procedural generalization from basic traffic building blocks. Currently the simulator is used to study the generalization of the driving agents trained from reinforcement learning. See <a href="">paper</a> for more detail.\ ', 
+    '为了更好地评估和改善基于强化学习的自动驾驶系统的泛化性能，我们提出了一个开放式、极易配置的模拟器：PGDrive！PGDrive可以通过过程生成技术生成无限多的地图\
     ，从而助力学习系统泛化性能的研究。欢迎访问 <a href="https://github.com/decisionforce/pgdrive">github.com/decisionforce/pgdrive</a> 来亲身感受！'],
-    ['We generate maps with these elementary road blocks:', '我们用了以下基础路块来生成地图：'],
-    ['Here are various generated maps:', '以下是生成的地图：'],
-    ['Agents trained with more maps show better test performance, showing that PGDrive endows agent with generality:', '在训练中见识过更多地图的智能体展现出了更优越的测试性能。这说明了我们的PGDrive赋予了智能体更强大的泛化能力！'],
-    ['The training code for the paper can be founded<a href="https://github.com/decisionforce/pgdrive-generalization-paper">here</a>.', '我们的训练代码在<a href="https://github.com/decisionforce/pgdrive-generalization-paper">这里</a>。'],
-    ['View our trained agents in the following video:', '请在如下视频中欣赏我们训练的智能体：'],
+    ['We first define the elementary road blocks as follows,','我们首先定义了如下的基础路块'],
+    ['we then follow the proposed algorithm of procedural generation to synthesize maps:', '随后我们用了过程生成的技术来生成地图：'],
+    ['We exhibit more generated maps as follows, which are further turned into interactive environments for reinforcement learning of end-to-end driving.', '我们展示了更多生成的地图，它们都可以被转换成强化学习算法可以与之交互的环境：'],
+    ['We show that when trained with more procedurally generated maps, the driving agents from reinofrcement learning have better generalization performance on unseen test maps, and can handle more complex scenarios. The detailed experimental results are in the paper. You can reproduce the experiment through <a href="https://github.com/decisionforce/pgdrive-generalization-paper">our generalization experiment code</a>.', '在训练中见识过更多地图的智能体展现出了更优越的测试性能。这说明了我们的PGDrive赋予了智能体更强大的泛化能力！'],
+    ['The demo video of the generalizable agent is shown as follows. You can run the agent on your local machine through the provided example in <a href="https://github.com/decisionforce/pgdrive">the simulator codebase</a>.',
+     '以下视频展示了我们的范化性智能体。您可以在自己的机器上通过<a href="https://github.com/decisionforce/pgdrive">模拟器代码</a>中提供的样例体验该智能体。'],
     ['Citation', '引用']
 ]
 var bar_corpus = [
-    ['Website', '网页'],
-    ['Github Repo', 'GitHub仓库'],
-    ['Documentation', '文档'],
-    ['Paper', '论文'],
-    ['切换中文', 'To English']
+    ['<b>Code</b>', '<b>代码</b>'],
+    ['<b>Documentation</b>', '<b>文档</b>'],
+    ['<b>Paper</b>', '<b>论文</b>']
 ]
+var vedio_corpus = [
+    ['<iframe src="https://www.youtube.com/embed/T368RveOY9g" frameborder=0\
+    style="position: absolute; top: 2.5%; left: 2.5%; width: 95%; height: 100%;"\
+    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"\
+    allowfullscreen></iframe>', 
+    '<iframe src="//player.bilibili.com/player.html?aid=373258795&bvid=BV15Z4y137cz&cid=271979816&page=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>'],
+]
+
+
 var lang_flag = 1;
 
 $(document).ready(function(){
-    $("button").click(function(){
+    $(".switch").click(function(){
         i = 0;
+        lang_flag=this.id;
         $(".title").each(
             function(){
                 $(this).html(title_corpus[i][lang_flag]);
@@ -59,7 +67,7 @@ $(document).ready(function(){
         );
 
         i=0
-        $(".institution div").each(
+        $(".institution").each(
             function(){
                 $(this).html(institution_corpus[i][lang_flag]);
                 i=i+1;
@@ -74,7 +82,6 @@ $(document).ready(function(){
             }
         );
 
-
         i=0
         $(".bar").each(
             function(){
@@ -82,6 +89,14 @@ $(document).ready(function(){
                 i=i+1;
             }
         );
+
+        // i=0
+        // $(".vedio").each(
+        //     function(){
+        //         $(this).html(vedio_corpus[i][lang_flag]);
+        //         i=i+1;
+        //     }
+        // );
 
         lang_flag = 1-lang_flag;
     });
