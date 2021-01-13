c161767d75fc95139996d3598ccb0537213cd798
Quanyi Liiiiii <785878978@qq.com>
2021-01-13 11:54:54 +0800

    Optimize render (#239)
    * Use LOD in PGDrive
    
    * format
    
    * fix bug and test all release and restore
    
    * format & save time consuming in physics world
    
    * format & save time consuming in physics world
    
    * adjust parameters
    
    * format.sh

---
+++ pgdrive/scene_manager/PgLOD.py
@@ -0,0 +1,53 @@
+from pgdrive.world.pg_world import PgWorld
+from pgdrive.utils.math_utils import norm
+
+
+class PgLOD:
+    """
+    Used to cull distant rendering object to improve rendering efficiency
+    TODO calculation efficiency can also be improved in the future
+    """
+    # Visualization cull
+    LOD_MAP_VIS_DIST = 300  # highly related to the render efficiency !
+    LOD_VEHICLE_VIS_DIST = 500
+
+    # Physics world cull, which can save the time used to do collision detection
+    LOD_MAP_PHYSICS_DIST = 50
+    LOD_VEHICLE_PHYSICS_DIST = 50
+
+    @classmethod
+    def cull_distant_blocks(cls, blocks: list, pos, pg_world: PgWorld):
+        # A distance based LOD rendering like GTA
+        for block in blocks:
+            if block.bounding_box[0] - cls.LOD_MAP_VIS_DIST < pos[0] < block.bounding_box[1] + cls.LOD_MAP_VIS_DIST and \
+                    block.bounding_box[2] - cls.LOD_MAP_VIS_DIST < \
+                    pos[1] < block.bounding_box[3] + cls.LOD_MAP_VIS_DIST:
+                if not block.node_path.hasParent():
+                    block.node_path.reparentTo(pg_world.worldNP)
+            else:
+                if block.node_path.hasParent():
+                    block.node_path.detachNode()
+            if block.bounding_box[0] - cls.LOD_MAP_PHYSICS_DIST < pos[0] < block.bounding_box[
+                1] + cls.LOD_MAP_PHYSICS_DIST and \
+                    block.bounding_box[2] - cls.LOD_MAP_PHYSICS_DIST < \
+                    pos[1] < block.bounding_box[3] + cls.LOD_MAP_PHYSICS_DIST:
+                block.dynamic_nodes.attach_to_physics_world(pg_world.physics_world.dynamic_world)
+            else:
+                block.dynamic_nodes.detach_from_physics_world(pg_world.physics_world.dynamic_world)
+
+    @classmethod
+    def cull_distant_traffic_vehicles(cls, vehicles: list, pos, pg_world: PgWorld):
+        # Cull distant vehicles
+        for v in vehicles:
+            v_p = v.position
+            if norm(v_p[0] - pos[0], v_p[1] - v_p[1]) < cls.LOD_VEHICLE_VIS_DIST:
+                if not v.node_path.hasParent():
+                    v.node_path.reparentTo(pg_world.pbr_worldNP)
+            else:
+                if v.node_path.hasParent():
+                    v.node_path.detachNode()
+
+            if norm(v_p[0] - pos[0], v_p[1] - v_p[1]) < cls.LOD_VEHICLE_PHYSICS_DIST:
+                v.dynamic_nodes.attach_to_physics_world(pg_world.physics_world.dynamic_world)
+            else:
+                v.dynamic_nodes.detach_from_physics_world(pg_world.physics_world.dynamic_world)
--- pgdrive/tests/test_pgdrive_env/vis_pgdrive_env.py
+++ pgdrive/tests/test_pgdrive_env/vis_pgdrive_env.py
@@ -7,14 +7,19 @@ setup_logger(True)
 
 class TestEnv(PGDriveEnv):
     def __init__(self):
+        """
+        TODO a small bug exists in scene 9 (30 blocks)
+        """
         super(TestEnv, self).__init__(
             {
                 "environment_num": 1,
-                "traffic_density": 0.1,
+                "traffic_density": 0.3,
                 "traffic_mode": "reborn",
                 "start_seed": 5,
                 "pg_world_config": {
                     "onscreen_message": True,
+                    # "debug_physics_world":True,
+                    "pstats": True
                 },
                 "image_source": "mini_map",
                 "manual_control": True,
@@ -23,9 +28,10 @@ class TestEnv(PGDriveEnv):
                 "steering_penalty": 0.0,
                 "decision_repeat": 5,
                 "rgb_clip": True,
+                # "debug":True,
                 "map_config": {
                     Map.GENERATE_METHOD: MapGenerateMethod.BIG_BLOCK_NUM,
-                    Map.GENERATE_PARA: 20,
+                    Map.GENERATE_PARA: 30,
                     Map.LANE_WIDTH: 3.5,
                     Map.LANE_NUM: 3,
                 }
@@ -40,7 +46,7 @@ if __name__ == "__main__":
     for i in range(1, 100000):
         o, r, d, info = env.step([0, 1])
         env.render()
-        if d:
-            print("Reset")
-            env.reset()
+        # if d:
+        #     print("Reset")
+        #     env.reset()
     env.close()
