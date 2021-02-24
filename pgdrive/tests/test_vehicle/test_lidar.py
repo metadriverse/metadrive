26473031d5e58aaca8fb56e28f4851e1486c676e
Quanyi Liiiiii <785878978@qq.com>
2021-02-24 15:01:18 +0800

    fix lidar bug and add it to unitest (#256)
    * fix lidar bug and add it to unitest
    
    * test_script
    
    * format
    
    * fix pytest bug

---
+++ pgdrive/tests/test_vehicle/test_lidar.py
@@ -0,0 +1,33 @@
+from pgdrive.envs.pgdrive_env import PGDriveEnv
+from pgdrive.scene_creator.ego_vehicle.vehicle_module.lidar import Lidar
+from pgdrive.utils import setup_logger
+
+
+class TestEnv(PGDriveEnv):
+    def __init__(self, config):
+        super(TestEnv, self).__init__(config)
+
+
+def test_lidar(render=False):
+    setup_logger(debug=True)
+    Lidar.enable_show = True
+    env = TestEnv({"use_render": render, "manual_control": render, "environment_num": 1, "traffic_density": 0.3})
+    try:
+        env.reset()
+        detect_vehicle = False
+        for i in range(1, 100000):
+            o, r, d, info = env.step([0, 1])
+
+            if len(env.vehicle.lidar.get_surrounding_vehicles()) != 0:
+                detect_vehicle = True
+
+            # env.render("Test: {}".format(i))
+            if d:
+                break
+        assert detect_vehicle, "Lidar detection failed"
+    finally:
+        env.close()
+
+
+if __name__ == "__main__":
+    test_lidar(render=True)
