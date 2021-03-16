d12a3b494b12fda898bb65f4672c664b99c38ce7
PENG Zhenghao <pengzh@ie.cuhk.edu.hk>
2021-03-16 16:30:30 +0800

    Allow building city (tiny change in BIG) (#289)
    * move place of the drawing function
    
    * setup a test script
    
    * WIP stop here
    
    * format
    
    * working in progess, bug happen in intersection.py
    
    * format
    
    * add tests
    
    * fix tests

--- pgdrive/tests/test_additional_envs/vis_build_city.py
+++ pgdrive/tests/test_additional_envs/vis_build_city.py
@@ -5,13 +5,14 @@ from pgdrive.utils import draw_top_down_map
 from pgdrive.world.pg_world import PGWorld
 
 if __name__ == '__main__':
-    map_config = dict(type="block_num", config=7)
+    num_blocks = 100
 
+    map_config = dict(type="block_num", config=num_blocks)
     world = PGWorld()
     map = CityMap(world, map_config)
     fig = draw_top_down_map(map, (1024, 1024))
     plt.imshow(fig, cmap="bone")
     plt.xticks([])
     plt.yticks([])
-    plt.title("Building a City!")
+    plt.title("Building a City with {} blocks!".format(num_blocks))
     plt.show()
