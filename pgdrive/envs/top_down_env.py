f4972003c0f9b186595822bb8067a6eb06540c4d
PENG Zhenghao <pengzh@ie.cuhk.edu.hk>
2021-04-03 16:28:58 +0800

    Allow to tune resolution in top-down env (#341)
    * update a new env to test minimal performance of PGDrive
    
    * update
    
    * update top-down
    
    * finish minimal environment
    
    * update
    
    * format

--- pgdrive/envs/top_down_env.py
+++ pgdrive/envs/top_down_env.py
@@ -9,7 +9,7 @@ class TopDownSingleFramePGDriveEnv(PGDriveEnv):
     def default_config(cls) -> PGConfig:
         config = PGDriveEnv.default_config()
         config["vehicle_config"]["lidar"] = {"num_lasers": 0, "distance": 0}  # Remove lidar
-        config.update({"frame_skip": 5, "frame_stack": 5, "post_stack": 5, "rgb_clip": True})
+        config.update({"frame_skip": 5, "frame_stack": 5, "post_stack": 5, "rgb_clip": True, "resolution_size": 100})
         return config
 
     def get_single_observation(self, _=None):
@@ -25,6 +25,7 @@ class TopDownPGDriveEnv(TopDownSingleFramePGDriveEnv):
             frame_stack=self.config["frame_stack"],
             post_stack=self.config["post_stack"],
             frame_skip=self.config["frame_skip"],
+            resolution=(self.config["resolution_size"], self.config["resolution_size"])
         )
 
 
@@ -33,7 +34,7 @@ class TopDownPGDriveEnvV2(PGDriveEnvV2):
     def default_config(cls) -> PGConfig:
         config = PGDriveEnvV2.default_config()
         config["vehicle_config"]["lidar"] = {"num_lasers": 0, "distance": 0}  # Remove lidar
-        config.update({"frame_skip": 5, "frame_stack": 1, "post_stack": 5, "rgb_clip": True})
+        config.update({"frame_skip": 5, "frame_stack": 1, "post_stack": 5, "rgb_clip": True, "resolution_size": 100})
         return config
 
     def get_single_observation(self, _=None):
@@ -44,6 +45,7 @@ class TopDownPGDriveEnvV2(PGDriveEnvV2):
             frame_stack=self.config["frame_stack"],
             post_stack=self.config["post_stack"],
             frame_skip=self.config["frame_skip"],
+            resolution=(self.config["resolution_size"], self.config["resolution_size"])
         )
 
 
@@ -65,7 +67,9 @@ if __name__ == '__main__':
     # env.close()
 
     # Test multi-channel frames
-    env = TopDownPGDriveEnvV2(dict(environment_num=1, map="XTO", traffic_density=0.1, frame_stack=5))
+    env = TopDownPGDriveEnvV2(
+        dict(environment_num=1, map="SS", traffic_density=0.5, frame_stack=5, frame_skip=5, resolution_size=64)
+    )
     # env = TopDownPGDriveEnv(dict(environment_num=1, map="XTO", traffic_density=0.1, frame_stack=5))
     # env = TopDownPGDriveEnv(dict(use_render=True, manual_control=True))
     env.reset()
@@ -73,11 +77,11 @@ if __name__ == '__main__':
         "road_network", "navigation", "target_vehicle", "past_pos", "traffic t", "traffic t-1", "traffic t-2",
         "traffic t-3", "traffic t-4"
     ]
-    for _ in range(20):
-        o, *_ = env.step([-0.05, 1])
+    for _ in range(40):
+        o, *_ = env.step([-0.00, 0.2])
         assert env.observation_space.contains(o)
     for _ in range(10000):
-        o, *_ = env.step([1, 1])
+        o, *_ = env.step([0, 1])
 
         fig, axes = plt.subplots(1, o.shape[-1], figsize=(15, 3))
 
