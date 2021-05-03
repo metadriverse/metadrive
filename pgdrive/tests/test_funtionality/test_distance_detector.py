8797bbf38eb51f9800873d63bcb8c30b5ff16ef8
Quanyi Li <785878978@qq.com>
2021-05-03 16:18:04 +0800

    Refactor traffic mgr (#434)
    * refactor
    
    * I can not fix detector mask error
    
    * format
    
    * fix test and fix camera switch bug
    
    * format
    
    * adjust test for lidar-detection
    
    * format
    
    * fix test error

--- pgdrive/tests/test_funtionality/test_distance_detector.py
+++ pgdrive/tests/test_funtionality/test_distance_detector.py
@@ -9,7 +9,7 @@ class TestEnv(PGDriveEnv):
         super(TestEnv, self).__init__(config)
 
 
-def test_lidar(render=False):
+def test_original_lidar(render=False):
     setup_logger(debug=True)
     env = TestEnv(
         {
@@ -57,5 +57,58 @@ def test_lidar(render=False):
         env.close()
 
 
+def test_lidar_with_mask(render=False):
+    setup_logger(debug=True)
+    env = TestEnv(
+        {
+            "use_render": render,
+            "manual_control": render,
+            "environment_num": 1,
+            "traffic_density": 0.3,
+            "vehicle_config": {
+                "show_lidar": True,
+                "side_detector": dict(num_lasers=2, distance=50),
+                "lane_line_detector": dict(num_lasers=2, distance=50),
+            },
+            "_disable_detector_mask": False
+        }
+    )
+    try:
+        env.reset()
+        vehicle = env.vehicle
+        v_config = env.config["vehicle_config"]
+        v_config["spawn_longitude"] = 80
+        v_config["spawn_lateral"] = 5
+        another_v = BaseVehicle(env.pg_world, v_config)
+        another_v.reset(env.current_map)
+        # for test
+        env.agent_manager._pending_objects[another_v.name] = another_v
+
+        objs = vehicle.side_detector.get_detected_objects() + vehicle.lane_line_detector.get_detected_objects()
+        yellow = 0
+        for obj in objs:
+            if obj.getNode().getName() == BodyName.Yellow_continuous_line:
+                yellow += 1
+        assert yellow == 2, "side detector and lane detector broken"
+        detect_traffic_vehicle = False
+        detect_base_vehicle = False
+        for i in range(1, 100000):
+            o, r, d, info = env.step([0, 1])
+            if len(vehicle.lidar.get_surrounding_vehicles()) > 4:
+                detect_traffic_vehicle = True
+            for hit in vehicle.lidar.get_detected_objects():
+                v = hit.getNode()
+                if v.hasPythonTag(BodyName.Base_vehicle):
+                    detect_base_vehicle = True
+            if d:
+                break
+        if not (detect_traffic_vehicle and detect_base_vehicle):
+            print("Lidar detection failed")
+        assert detect_traffic_vehicle and detect_base_vehicle, "Lidar detection failed"
+    finally:
+        env.close()
+
+
 if __name__ == "__main__":
-    test_lidar(render=False)
+    test_lidar_with_mask(render=False)
+    test_original_lidar(render=False)
