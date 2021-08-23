d6dfea39a51078a69f170ef3ae41518a8f6e88a0
Quanyi Li <785878978@qq.com>
2021-08-23 23:33:27 +0800

    Move env (#534)
    * safety reward is same as pgdrive
    
    * move env to  drivingforce
    
    * fix test

--- pgdrive/tests/test_env/local_test_pgdrive_rgb_depth.py
+++ pgdrive/tests/test_env/local_test_pgdrive_rgb_depth.py
@@ -1,5 +1,21 @@
 from pgdrive.envs.pgdrive_env import PGDriveEnv
-from pgdrive.tests.test_env.test_pgdrive_env import _act
+from pgdrive.constants import TerminationState
+import numpy as np
+
+info_keys = [
+    "cost", "velocity", "steering", "acceleration", "step_reward", TerminationState.CRASH_VEHICLE,
+    TerminationState.OUT_OF_ROAD, TerminationState.SUCCESS
+]
+
+
+def _act(env, action):
+    assert env.action_space.contains(action)
+    obs, reward, done, info = env.step(action)
+    assert env.observation_space.contains(obs)
+    assert np.isscalar(reward)
+    assert isinstance(info, dict)
+    for k in info_keys:
+        assert k in info
 
 
 def test_pgdrive_env_rgb():
