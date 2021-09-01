4499088dc13a8c7c4f726d36ec9d9434fdd04bad
PENG Zhenghao <pengzh@ie.cuhk.edu.hk>
2021-09-01 21:27:46 +0800

    Update documents on RL environments (#33)
    * update the envs
    
    * finish environments
    
    * finish environments docs
    
    * format

--- metadrive/__init__.py
+++ metadrive/__init__.py
@@ -1,2 +1,4 @@
 import metadrive.register
-from metadrive.envs import MetaDriveEnv, TopDownMetaDrive, TopDownSingleFrameMetaDriveEnv, TopDownMetaDriveEnvV2
+from metadrive.envs import MetaDriveEnv, TopDownMetaDrive, TopDownSingleFrameMetaDriveEnv, TopDownMetaDriveEnvV2, \
+    SafeMetaDriveEnv, ArgoverseEnv, MultiAgentRoundaboutEnv, MultiAgentIntersectionEnv, MultiAgentParkingLotEnv, \
+    MultiAgentTollgateEnv, MultiAgentBottleneckEnv, MultiAgentMetaDrive
