1e397d00d586182aaeb4bca20697b77756a414e0
lqy0057 <785878978@qq.com>
2021-09-17 16:45:18 +0800

    model

--- metadrive/component/vehicle_model/bicycle_model.py
+++ metadrive/component/vehicle_model/bicycle_model.py
@@ -1,46 +1,52 @@
-def plant_model(self, dt, *control):
-    # in this model, we formulate the car's dynamic model as Bicycle model
-    #
-    # information that should be tracked in 'state'
-    # x, y: geometric coordinate
-    # v : velocity
-    # phi:  heading angle
-    # beta: velocity angle
+import numpy as np
 
-    # things need to be finetuned
-    # mass
-    # mu*g: a = f/m + \mu * mg
-    #
 
-    [x, y, v, phi, beta] = self.state
-    pedal, steering = control[0]
-    pedal *= 500
-    steering *= 40
-    new_beta = np.arctan(0.5 * np.tan(steering / 180 * np.pi))
+class BicycleModel:
+    """
+    This model can be used to predict next state
+    """
 
-    if pedal < 0:
-        pedal *= 3
+    def __init__(self):
+        self.state = dict(x=0, y=0, speed=0, heading_theta=0, velocity_dir=0)
 
-    a = pedal / 500 * 3
-    af = .5
+    def reset(self, x, y, speed, heading_theta, velocity_dir):
+        """
+        heading_theta, velocity_dir in radian
+        """
+        self.state = dict(x=x, y=y, speed=speed, heading_theta=heading_theta, velocity_dir=velocity_dir)
 
-    new_v = 0
-    if v > 1e-5 or a > af:
-        new_v = v + (a - af) * dt
-        if v * new_v < 0:
-            new_v = 0
+    def predict(self, dt, control):
+        """
+        In this model, we formulate the car's dynamic model as Bicycle model
+        things need to be finetuned
+        mass
+        mu*g: a = f/m + \mu * mg
+        """
+        x = self.state["x"]
+        y = self.state["y"]
+        v = self.state["speed"]
+        phi = self.state["heading_theta"]
+        beta = self.state["velocity_dir"]
+        pedal, steering = control[0], control[1]
+        pedal *= 500
+        steering *= 40
+        new_beta = np.arctan(0.5 * np.tan(steering / 180 * np.pi))
 
-    new_phi = phi + v / 2 / 2 * np.tan(steering / 180 * np.pi) * dt
-    new_x = x + v * np.cos(phi + beta) * dt
-    new_y = y + v * np.sin(phi + beta) * dt
-    new_state = [new_x, new_y, new_v, new_phi, new_beta]
-    self.state = new_state
-    # self.chassis_np2.setPos(50, 0, 1)
-    print('---------------', pedal, steering, self.state)
-    self.chassis_np2.setPos(self.state[0], self.state[1], 1)
-    self.chassis_np2.setHpr(90 + phi / np.pi * 180, 0, 0)
+        if pedal < 0:
+            pedal *= 3
 
-    # print('p s %d %d %.4f %.4f,'% (pedal, steering, beta, phi))
-    # print('MPC model %.4f %.4f'%(off/dt,numpy.linalg.norm(new_state[2])))
-    # print('MPC acceleration %.4f ' %(a))
-    return new_state
+        a = pedal / 500 * 3
+        af = .5
+
+        new_v = 0
+        if v > 1e-5 or a > af:
+            new_v = v + (a - af) * dt
+            if v * new_v < 0:
+                new_v = 0
+
+        new_phi = phi + v / 2 / 2 * np.tan(steering / 180 * np.pi) * dt
+        new_x = x + v * np.cos(phi + beta) * dt
+        new_y = y + v * np.sin(phi + beta) * dt
+        new_state = dict(x=new_x, y=new_y, speed=new_v, heading_theta=new_phi, velocity_dir=new_beta)
+        self.state = new_state
+        return new_state
