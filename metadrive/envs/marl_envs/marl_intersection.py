409926e84fc751e9c6deb569542ced789e65c026
Quanyi Li <785878978@qq.com>
2021-12-10 15:01:38 +0800

    Register MetaDrive in gym environment registry (#110)
    * fix rigister
    
    * format

--- metadrive/envs/marl_envs/marl_intersection.py
+++ metadrive/envs/marl_envs/marl_intersection.py
@@ -18,7 +18,7 @@ MAIntersectionConfig = dict(
         -Road(InterSection.node(1, 1, 0), InterSection.node(1, 1, 1)),
         -Road(InterSection.node(1, 2, 0), InterSection.node(1, 2, 1)),
     ],
-    num_agents=12,
+    num_agents=20,
     map_config=dict(exit_length=60, lane_num=2),
     top_down_camera_initial_x=80,
     top_down_camera_initial_y=0,
@@ -211,7 +211,7 @@ def _vis():
             "debug": True,
             "allow_respawn": False,
             "manual_control": True,
-            "num_agents": 2,
+            "num_agents": 20,
             "delay_done": 2,
         }
     )
