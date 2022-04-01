926041eb32dd2b9f3dbbb7c9ad011a18150201da
lqy0057 <785878978@qq.com>
2022-04-01 19:19:40 +0800

    add fps test script but only for local test

--- metadrive/tests/test_functionality/test_fps.py
+++ metadrive/tests/test_functionality/test_fps.py
@@ -6,7 +6,7 @@ from metadrive import MetaDriveEnv
 from metadrive.utils import setup_logger
 
 
-def test_fps():
+def _test_fps():
     print("Start to profile the efficiency of MetaDrive with 1000 maps and ~8 vehicles!")
     try:
         setup_logger(debug=False)
@@ -24,10 +24,10 @@ def test_fps():
             if d:
                 env.reset()
                 vehicle_num.append(len(env.engine.traffic_manager.vehicles))
-        assert total_steps / (time.time() - start) > 160
+        assert total_steps / (time.time() - start) > 200
     finally:
         env.close()
 
 
 if __name__ == '__main__':
-    test_fps()
+    _test_fps()
