02882fc2c728314a5e84d815e4bf5ec762ec31ef
pengzhenghao <pzh@cs.ucla.edu>
2022-10-29 21:15:27 -0700

    better print

--- metadrive/policy/manual_control_policy.py
+++ metadrive/policy/manual_control_policy.py
@@ -48,7 +48,8 @@ class ManualControlPolicy(EnvInputPolicy):
                 return expert(self.engine.current_track_vehicle)
         except (ValueError, AssertionError):
             # if observation doesn't match, fall back to manual control
-            pass
+            print("Current observation does not match the format that expert can accept.")
+            self.toggle_takeover()
 
         is_track_vehicle = self.engine.agent_manager.get_agent(agent_id) is self.engine.current_track_vehicle
         not_in_native_bev = (self.engine.main_camera is None) or (not self.engine.main_camera.is_bird_view_camera())
