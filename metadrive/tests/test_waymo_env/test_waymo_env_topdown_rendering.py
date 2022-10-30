421021aaf735e68818bb66acecb6332a47fa4a06
pengzhenghao <pzh@cs.ucla.edu>
2022-10-29 21:55:10 -0700

    add a test for Waymo rendering

---
+++ metadrive/tests/test_waymo_env/test_waymo_env_topdown_rendering.py
@@ -0,0 +1,20 @@
+from metadrive.envs.real_data_envs.waymo_env import WaymoEnv
+
+
+def test_waymo_env_topdown_rendering():
+    for env in [
+        WaymoEnv({"case_num": 3})
+    ]:
+        try:
+            o = env.reset()
+            for i in range(1, 1000):
+                o, r, d, info = env.step([1.0, 0.])
+                env.render("topdown")
+                if d:
+                    break
+        finally:
+            env.close()
+
+
+if __name__ == "__main__":
+    test_waymo_env_topdown_rendering()
