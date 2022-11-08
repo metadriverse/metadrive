3f4e48c87b92d40afcb43228f8d14a060321e97b
QuanyiLi <quanyili0057@gmail.com>
2022-11-08 14:20:44 +0000

    test script

--- metadrive/tests/test_functionality/test_save_replay_scenario.py
+++ metadrive/tests/test_functionality/test_save_replay_scenario.py
@@ -6,6 +6,16 @@ from metadrive.envs.safe_metadrive_env import SafeMetaDriveEnv
 from metadrive.manager.traffic_manager import TrafficMode
 from metadrive.policy.idm_policy import IDMPolicy
 from metadrive.utils import setup_logger
+from metadrive.utils.math_utils import norm
+
+
+def assert_equal_pos(dict_1, dict_2):
+    for name, pos in dict_1.items():
+        assert name in dict_2, "No such vehicle in dict 2: {}".format(name)
+        pos_2 = dict_2[name]
+        difference = pos - pos_2
+        diff = norm(difference[0], difference[1])
+        assert diff < 0.00001, "pos mismatch for vehicle: {}, distance: {}".format(name, diff)
 
 
 def _test_save_scenario(vis=False):
@@ -32,48 +42,60 @@ def _test_save_scenario(vis=False):
             }
         }
     )
-    try:
-        o = env.reset()
-        for i in range(1, 100000 if vis else 2000):
-            o, r, d, info = env.step([0, 1])
-            # if vis:
-            #     env.render(mode="top_down", road_color=(35, 35, 35))
-            if d:
-                epi_info = env.engine.record_manager.get_episode_metadata()
-                break
-        env.close()
-        env = SafeMetaDriveEnv(
-            {
-                "accident_prob": 0.8,
-                "environment_num": 1,
-                "traffic_density": 0.1,
-                "start_seed": 1000,
-                "manual_control": True,
-                "debug": True,
-                "use_render": vis,
-                "agent_policy": IDMPolicy,
-                "traffic_mode": TrafficMode.Trigger,
-                "record_episode": save_episode,
-                "map_config": {
-                    BaseMap.GENERATE_TYPE: MapGenerateMethod.BIG_BLOCK_SEQUENCE,
-                    BaseMap.GENERATE_CONFIG: "CrCSC",
-                    BaseMap.LANE_WIDTH: 3.5,
-                    BaseMap.LANE_NUM: 3,
-                }
+    # try:
+    positions_1 = []
+    o = env.reset()
+    positions_1.append({v.name: v.position for v in env.engine.traffic_manager.spawned_objects.values()})
+    for i in range(1, 100000 if vis else 2000):
+        o, r, d, info = env.step([0, 1])
+        positions_1.append({v.name: v.position for v in env.engine.traffic_manager.spawned_objects.values()})
+        # if vis:
+        #     env.render(mode="top_down", road_color=(35, 35, 35))
+        if d:
+            epi_info = env.engine.record_manager.get_episode_metadata()
+            break
+    env.close()
+    positions_1.reverse()
+    env = SafeMetaDriveEnv(
+        {
+            "accident_prob": 0.8,
+            "environment_num": 1,
+            "traffic_density": 0.1,
+            "start_seed": 1000,
+            "manual_control": True,
+            "debug": True,
+            "use_render": vis,
+            "agent_policy": IDMPolicy,
+            "traffic_mode": TrafficMode.Trigger,
+            "record_episode": save_episode,
+            "map_config": {
+                BaseMap.GENERATE_TYPE: MapGenerateMethod.BIG_BLOCK_SEQUENCE,
+                BaseMap.GENERATE_CONFIG: "CrCSC",
+                BaseMap.LANE_WIDTH: 3.5,
+                BaseMap.LANE_NUM: 3,
             }
-        )
-        env.config["replay_episode"] = epi_info
-        env.config["record_episode"] = False
-        env.config["only_reset_when_replay"] = True
-        o = env.reset()
-        for i in range(1, 100000 if vis else 2000):
-            o, r, d, info = env.step([0, 1])
-            # if vis:
-            #     env.render(mode="top_down", )
-            if info.get("replay_done", False):
-                break
-    finally:
-        env.close()
+        }
+    )
+    env.config["replay_episode"] = epi_info
+    env.config["record_episode"] = False
+    env.config["only_reset_when_replay"] = True
+    o = env.reset()
+    position = positions_1.pop()
+    position = {env.engine.replay_manager.record_name_to_current_name[key]: v for key, v in position.items()}
+    current_position = {v.name: v.position for v in env.engine.traffic_manager.spawned_objects.values()}
+    assert_equal_pos(position, current_position)
+    for i in range(1, 100000 if vis else 2000):
+        o, r, d, info = env.step([0, 1])
+        position = positions_1.pop()
+        position = {env.engine.replay_manager.record_name_to_current_name[key]: v for key, v in position.items()}
+        current_position = {v.name: v.position for v in env.engine.traffic_manager.spawned_objects.values()}
+        assert_equal_pos(position, current_position)
+        # if vis:
+        #     env.render(mode="top_down", )
+        if info.get("replay_done", False):
+            break
+    # finally:
+    #     env.close()
 
 
 if __name__ == "__main__":
