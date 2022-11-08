2ad93895225d7db8934b2b216392cf3816199a4d
QuanyiLi <quanyili0057@gmail.com>
2022-11-08 15:28:33 +0000

    format

--- metadrive/tests/test_functionality/test_scenario_randomness.py
+++ metadrive/tests/test_functionality/test_scenario_randomness.py
@@ -12,7 +12,7 @@ from metadrive.utils.math_utils import norm
 def assert_equal_pos(data_1, data_2):
     assert len(data_1) == len(data_2)
     for i in range(len(data_1)):
-        difference = data_1[i]-data_2[i]
+        difference = data_1[i] - data_2[i]
         diff = norm(difference[0], difference[1])
         assert diff < 0.00001, "pos mismatch for vehicle: {}, distance: {}".format(i, diff)
 
@@ -20,31 +20,33 @@ def assert_equal_pos(data_1, data_2):
 def test_scenario_randomness(vis=False):
     setup_logger(True)
     cfg = {
-            "accident_prob": 0.8,
-            "environment_num": 1,
-            "traffic_density": 0.1,
-            "start_seed": 1000,
-            "manual_control": True,
-            "use_render": vis,
-            "agent_policy": IDMPolicy,
-            "traffic_mode": TrafficMode.Trigger,
-            "record_episode": False,
-            "map_config": {
-                BaseMap.GENERATE_TYPE: MapGenerateMethod.BIG_BLOCK_SEQUENCE,
-                BaseMap.GENERATE_CONFIG: "CrCSC",
-                BaseMap.LANE_WIDTH: 3.5,
-                BaseMap.LANE_NUM: 3,
-            }
+        "accident_prob": 0.8,
+        "environment_num": 1,
+        "traffic_density": 0.1,
+        "start_seed": 1000,
+        "manual_control": True,
+        "use_render": vis,
+        "agent_policy": IDMPolicy,
+        "traffic_mode": TrafficMode.Trigger,
+        "record_episode": False,
+        "map_config": {
+            BaseMap.GENERATE_TYPE: MapGenerateMethod.BIG_BLOCK_SEQUENCE,
+            BaseMap.GENERATE_CONFIG: "CrCSC",
+            BaseMap.LANE_WIDTH: 3.5,
+            BaseMap.LANE_NUM: 3,
         }
+    }
 
     env = SafeMetaDriveEnv(cfg)
     try:
         positions_1 = []
         o = env.reset()
-        positions_1.append([env.vehicle.position]+[v.position for v in env.engine.traffic_manager.traffic_vehicles])
+        positions_1.append([env.vehicle.position] + [v.position for v in env.engine.traffic_manager.traffic_vehicles])
         for i in range(1, 100000 if vis else 2000):
             o, r, d, info = env.step([0, 1])
-            positions_1.append([env.vehicle.position] + [v.position for v in env.engine.traffic_manager.traffic_vehicles])
+            positions_1.append(
+                [env.vehicle.position] + [v.position for v in env.engine.traffic_manager.traffic_vehicles]
+            )
             if d:
                 break
         env.close()
