5bab5bdaf06f5c037f539b8d1197bb8e22e652b7
pengzhenghao <pzh@cs.ucla.edu>
2022-12-01 20:28:42 -0800

    change

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 packages = find_namespace_packages(
-    exclude=("docs", "docs.*", "documentation", "documentation.*", "metadrive.assets.*", "build.*"))
+    exclude=("docs", "docs.*", "documentation", "documentation.*", "build.*"))
 print("We will install the following packages: ", packages)
 
 """ ===== Remember to modify the PG_EDITION at first ====="""
