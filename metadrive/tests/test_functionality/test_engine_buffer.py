3235610451a4843c6b3587b4c00ef650baa21482
pengzhenghao <pzh@cs.ucla.edu>
2022-12-20 16:11:06 -0800

    upload some test code

--- metadrive/tests/test_functionality/test_engine_buffer.py
+++ metadrive/tests/test_functionality/test_engine_buffer.py
@@ -2,13 +2,13 @@ from metadrive.envs.marl_envs import MultiAgentRoundaboutEnv
 
 
 def test_engine_buffer():
-    env = MultiAgentRoundaboutEnv({"num_buffering_objects": 0})
+    env = MultiAgentRoundaboutEnv({"num_buffering_objects": 0, "debug": True})
     try:
         for i in range(1, 30):
             env.reset()
     finally:
         env.close()
-    env = MultiAgentRoundaboutEnv({"num_buffering_objects": 200})
+    env = MultiAgentRoundaboutEnv({"num_buffering_objects": 200, "debug": True})
     try:
         for i in range(1, 30):
             env.reset()
