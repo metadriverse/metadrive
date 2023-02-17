dee461e16945dd1bcd0abc7c9bc8bef848ac2191
QuanyiLi <quanyili0057@gmail.com>
2023-02-17 17:42:09 +0000

    pedestrain speed

--- metadrive/component/traffic_participants/pedestrian.py
+++ metadrive/component/traffic_participants/pedestrian.py
@@ -1,15 +1,13 @@
-from metadrive.component.traffic_participants.base_traffic_participant import BaseTrafficParticipant
-from typing import Tuple
-
-from panda3d.bullet import BulletBoxShape
-from panda3d.bullet import BulletCylinderShape
-
-from metadrive.component.static_object.base_static_object import BaseStaticObject
 from direct.actor.Actor import Actor
+from panda3d.bullet import BulletCylinderShape
+from panda3d.core import LVector3
+
+from metadrive.component.traffic_participants.base_traffic_participant import BaseTrafficParticipant
 from metadrive.constants import BodyName
 from metadrive.constants import CollisionGroup
 from metadrive.engine.asset_loader import AssetLoader
 from metadrive.engine.physics_node import BaseRigidBodyNode
+from metadrive.utils.math_utils import norm
 
 
 class Pedestrian(BaseTrafficParticipant):
@@ -20,9 +18,11 @@ class Pedestrian(BaseTrafficParticipant):
     RADIUS = 0.3
     HEIGHT = 1.75
 
-    _MODEL = None
+    _MODEL = {}
 
-    def __init__(self, position, heading_theta, random_seed):
+    SPEED_LIST = [0.1, 0.6, 1.1, 1.6]
+
+    def __init__(self, position, heading_theta, random_seed=None):
         super(Pedestrian, self).__init__(position, heading_theta, random_seed)
 
         n = BaseRigidBodyNode(self.name, self.NAME)
@@ -31,15 +31,61 @@ class Pedestrian(BaseTrafficParticipant):
 
         self.body.addShape(BulletCylinderShape(self.RADIUS, self.HEIGHT))
         self.body.setIntoCollideMask(self.COLLISION_GROUP)
-        self.set_static(True)
+        # self.set_static(True)
         self.animation_controller = None
+        self.current_speed_model = 0.1
         if self.render:
-            # if Pedestrian._MODEL is None:
+            if len(Pedestrian._MODEL) == 0:
+                self._init_pedestrian_model()
+            Pedestrian._MODEL[self.current_speed_model].instanceTo(self.origin)
+
+    @classmethod
+    def _init_pedestrian_model(cls):
+        for idx, speed in enumerate(cls.SPEED_LIST):
             model = Actor(AssetLoader.file_path("models", "pedestrian", "scene.gltf"))
             # model: Actor = self._MODEL
             model.setScale(0.01)
-            model.setPos(0, 0, -self.HEIGHT / 2)
-            model.reparentTo(self.origin)
-            # self.animation_controller = model.get_anim_control("Take 001")
-            # self.animation_controller.setPlayRate(1)
-            # self.animation_controller.loop("Take 001")
+            model.setH(model.getH() + 90)
+            model.setPos(0, 0, -cls.HEIGHT / 2)
+            Pedestrian._MODEL[speed] = model
+            if idx == 0:
+                pass
+            else:
+                animation_controller = model.get_anim_control("Take 001")
+                animation_controller.setPlayRate(1 * speed / cls.SPEED_LIST[-1])
+                animation_controller.loop("Take 001")
+
+    def set_velocity(self, direction: list, value=None, in_local_frame=False):
+        if in_local_frame:
+            from metadrive.engine.engine_utils import get_engine
+            engine = get_engine()
+            direction = LVector3(*direction, 0.)
+            direction[1] *= -1
+            ret = engine.worldNP.getRelativeVector(self.origin, direction)
+            # no need for spinning 90 degree
+            direction = ret
+        speed = (norm(direction[0], direction[1]) + 1e-6)
+        if value is not None:
+            norm_ratio = value / speed
+        else:
+            norm_ratio = 1
+
+        if self.render:
+            speed_model_index = self.get_speed_model(target_speed=speed if value is None else value)
+            if speed_model_index != self.current_speed_model:
+                child = self.origin.getChildren()
+                child.detach()
+                Pedestrian._MODEL[speed_model_index].instanceTo(self.origin)
+                self.current_speed_model = speed_model_index
+
+        self._body.setLinearVelocity(
+            LVector3(direction[0] * norm_ratio, direction[1] * norm_ratio,
+                     self._body.getLinearVelocity()[-1])
+        )
+
+    @staticmethod
+    def get_speed_model(target_speed):
+        for speed in Pedestrian.SPEED_LIST:
+            if target_speed < speed:
+                return speed
+        return Pedestrian.SPEED_LIST[-1]
