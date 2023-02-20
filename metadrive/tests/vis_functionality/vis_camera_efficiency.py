388f36a8d29258f413df1336aa75aa54830e1e1e
QuanyiLi <quanyili0057@gmail.com>
2023-02-20 20:42:51 +0000

    add test script

---
+++ metadrive/tests/vis_functionality/vis_camera_efficiency.py
@@ -0,0 +1,47 @@
+import time
+import cv2
+
+import numpy as np
+from metadrive import MetaDriveEnv
+
+
+def _test_rgb_camera_as_obs(render=False):
+    env = MetaDriveEnv(
+        dict(
+            environment_num=1,
+            start_seed=1010,
+            traffic_density=0.0,
+            offscreen_render=True,
+            use_render=False,
+            vehicle_config=dict(image_source="rgb_camera", rgb_camera=(1200, 800)),
+
+            show_interface=False,
+            show_logo=False,
+            show_fps=False,
+        )
+    )
+    obs = env.reset()
+    action = [0.0, 0.1]
+    start = time.time()
+    for i in range(20000):
+        o, r, d, _ = env.step(action)
+        # engine = env.engine
+        # if engine.episode_step <= 1:
+        #     engine.graphicsEngine.renderFrame()
+        # origin_img = engine.win.getDisplayRegion(0).getScreenshot()
+        # v = memoryview(origin_img.getRamImage).tolist()
+        # img = np.array(v)
+        # img = img.reshape((origin_img.getYSize(), origin_img.getXSize(), 4))
+        # img = img[::-1]
+        # img = img[..., :-1]
+        # img = img/255
+        if render:
+            cv2.imshow("window", o["image"][..., -1])
+            cv2.waitKey(1)
+        #
+        if i % 100 == 0 and i != 0:
+            print("FPS: {}".format(i / (time.time() - start)))
+
+
+if __name__ == "__main__":
+    _test_rgb_camera_as_obs(True)
--- metadrive/tests/vis_functionality/vis_cuda_image.py
+++ metadrive/tests/vis_functionality/vis_cuda_image.py
@@ -1,3 +1,9 @@
+import time
+import cv2
+from panda3d.core import Texture, GraphicsOutput, GraphicsStateGuardianBase
+
+import numpy as np
+
 from metadrive.component.pgblock.curve import Curve
 from metadrive.component.pgblock.first_block import FirstPGBlock
 from metadrive.component.pgblock.intersection import InterSection
@@ -5,20 +11,57 @@ from metadrive.component.road_network.node_road_network import NodeRoadNetwork
 from metadrive.tests.vis_block.vis_block_base import TestBlock
 
 if __name__ == "__main__":
-    engine = TestBlock()
+    engine = TestBlock(window_type="offscreen")
     from metadrive.engine.asset_loader import initialize_asset_loader
+
     initialize_asset_loader(engine)
+
     global_network = NodeRoadNetwork()
     first = FirstPGBlock(global_network, 3.0, 2, engine.render, engine.world, 20)
+
     intersection = InterSection(3, first.get_socket(0), global_network, 1)
     print(intersection.construct_block(engine.render, engine.world))
-    engine.show_bounding_box(global_network)
-    while True:
-        engine = get_engine()
-        assert engine.main_camera.current_track_vehicle is vehicle, "Tracked vehicle mismatch"
-        if engine.episode_step <= 1:
-            engine.graphicsEngine.renderFrame()
-        origin_img = engine.win.getDisplayRegion(0).getScreenshot()
-        v = memoryview(origin_img.getRamImage()).tolist()
 
+    id = 4
+    for socket_idx in range(intersection.SOCKET_NUM):
+        block = Curve(id, intersection.get_socket(socket_idx), global_network, id)
+        block.construct_block(engine.render, engine.world)
+        id += 1
+
+    intersection = InterSection(id, block.get_socket(0), global_network, 1)
+    intersection.construct_block(engine.render, engine.world)
+
+    engine.show_bounding_box(global_network)
+
+    # set texture
+    my_texture = Texture()
+    my_texture.setMinfilter(Texture.FTLinear)
+    my_texture.setFormat(Texture.FRgba32)
+
+    start_time = time.time()
+    engine.taskMgr.step()
+    engine.win.add_render_texture(my_texture, GraphicsOutput.RTMCopyRam)
+
+
+    for i in range(10000):
         engine.taskMgr.step()
+        origin_img = engine.win.getDisplayRegion(0).getScreenshot()
+        img = np.frombuffer(origin_img.getRamImage().getData(), dtype=np.uint8)
+        img = img.reshape((origin_img.getYSize(), origin_img.getXSize(), 4))
+        img = img[::-1]
+        img = img[..., :-1]
+
+        # gsg = GraphicsStateGuardianBase.getDefaultGsg()
+        # texture_context = my_texture.prepareNow(0, gsg.prepared_objects, gsg)
+        # # texture_context = my_texture.prepare(0)
+        # identifier = texture_context.getNativeId()
+
+        # img = np.frombuffer(my_texture.getRamImage().getData(), dtype=np.uint8)
+        # img = img.reshape((my_texture.getYSize(), my_texture.getXSize(), 4))
+        # img = img[::-1]
+        # img = img[..., :-1]
+        # cv2.imshow("window", img)
+        # cv2.waitKey(1)
+
+        if i % 1000 == 0 and i != 0:
+            print("FPS: {}".format(i / (time.time() - start_time)))
