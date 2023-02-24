989cf4fc6546d5f3ef7c22c2fff02262888bc5fa
pengzhenghao <pzh@cs.ucla.edu>
2023-02-23 23:57:56 -0800

    optimize demo

--- metadrive/examples/generate_video_for_bev_and_interface.py
+++ metadrive/examples/generate_video_for_bev_and_interface.py
@@ -1,10 +1,19 @@
+"""
+This file will run environment for one episode and generate two videos:
+
+example_video_RANDOM-STRING/0_bev.mp4
+example_video_RANDOM-STRING/0_interface.mp4
+
+This file demonstrates how to use API to generate high-resolution video for demo.
+"""
 import os
-import os.path as osp
 import uuid
 
 import cv2
 import matplotlib.pyplot as plt
 import pygame
+
+from metadrive.envs import MetaDriveEnv
 
 
 def plot(d):
@@ -14,24 +23,13 @@ def plot(d):
 
 
 class VideoRecorder:
-
-    def __init__(self, video_name, code, height, width):
+    def __init__(self, video_name, code, height, width, fps=40):
         """
         code=mp4v, avc1, x264, h264 etc.
         """
         assert video_name.endswith(".mp4")
-        # assert len(image_list) > 0
-        # frame = cv2.imread(os.path.join(image_folder, images[0]))
-        # frame = image_list[0]
-        # height, width, layers = frame.shape
-        video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*code), 40, (width, height))
+        video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*code), fps, (width, height))
         self.video_writer = video
-        # for image in tqdm(image_list, desc="Writing Video"):
-
-        # Change color
-        # image = image[..., ::-1]
-
-        # video.write(image)
 
     def write(self, image):
         self.video_writer.write(image[..., ::-1])
@@ -45,77 +43,29 @@ class VideoRecorder:
 
 
 if __name__ == '__main__':
-    ckpt_path = osp.join(
-        # "PPO_MetaDriveEnv_87e53_00005_5_environment_num=3,start_seed=5000,traffic_density=0.3000_2023-02-22_22-10-18/checkpoint_000460")
-        "PPO_MetaDriveEnv_87e53_00001_1_environment_num=3,start_seed=5000,traffic_density=0.1000_2023-02-22_22-09-04/checkpoint_000500"
-        # "PPO_MetaDriveEnv_87e53_00001_1_environment_num=3,start_seed=5000,traffic_density=0.1000_2023-02-22_22-09-04/checkpoint_000500"
-        # "PPO_MetaDriveEnv_87e53_00006_6_environment_num=5,start_seed=5000,traffic_density=0.3000_2023-02-22_22-10-34/checkpoint_000500"
-        # "PPO_MetaDriveEnv_87e53_00002_2_environment_num=5,start_seed=5000,traffic_density=0.1000_2023-02-22_22-09-22/checkpoint_000500"
-        # "PPO_MetaDriveEnv_87e53_00007_7_environment_num=1000,start_seed=5000,traffic_density=0.3000_2023-02-22_22-10-50/checkpoint_000500"
-        # "PPO_MetaDriveEnv_87e53_00000_0_environment_num=1,start_seed=5000,traffic_density=0.1000_2023-02-22_22-08-46/checkpoint_000500"
-    )
-    traffic_density = 0.2
-    # traffic_density = 0.1
-    environment_num = 5
-    # environment_num = 100
+    environment_num = 1  # Will run each seed for 1 episode.
     start_seed = 100
-    # start_seed = 0
-    # num_ep = 10
     generate_video = True
 
     if generate_video:
-        # folder_name = "td0.3_training_visualization_video_{}".format(str(uuid.uuid4())[:6])
-        # folder_name = "td0.3_test_visualization_video_{}".format(str(uuid.uuid4())[:6])
-        # folder_name = "td0.1_test_visualization_video_{}".format(str(uuid.uuid4())[:6])
-        # folder_name = "td0.1_training_video_{}".format(str(uuid.uuid4())[:6])
-        # folder_name = "td0.3_env5_training_video_{}".format(str(uuid.uuid4())[:6])
-        # folder_name = "td0.1_env5_training_video_{}".format(str(uuid.uuid4())[:6])
-        # folder_name = "expert_training_video_{}".format(str(uuid.uuid4())[:6])
-        # folder_name = "td0.1_env1_test_video_{}".format(str(uuid.uuid4())[:6])
-        folder_name = "td0.1_env5_test_video_{}".format(str(uuid.uuid4())[:6])
+        folder_name = "example_video_{}".format(str(uuid.uuid4())[:6])
 
-    # ==========
-    import torch
-    from ray.rllib.algorithms.ppo import PPO
-    from metadrive.envs import MetaDriveEnv
-
-    agent = PPO(dict(
-
-        # ===== Training Environment =====
-        # Train the policies in scenario sets with different number of scenarios.
-        env=MetaDriveEnv,
-        env_config=dict(
-            traffic_density=traffic_density,
+    env=MetaDriveEnv(dict(
             environment_num=environment_num,
             start_seed=start_seed,
             random_traffic=False,
             use_render=True,
             window_size=(1600, 1200),
             crash_vehicle_done=False,
-
-            # manual_control=True,
-        ),
-
-        # ===== Framework =====
-        framework="torch",
-
-        num_workers=0,
-
-        # ===== Resources Specification =====
-        num_gpus=0.25 if torch.cuda.is_available() else 0,
-        num_cpus_per_worker=0.2,
-        num_cpus_for_driver=0.5
+            manual_control=True,  # For using expert policy. You don't need to control it.
     ))
-    agent.restore(ckpt_path)
-    env = agent.workers.local_worker().env
 
     ep_count = 0
     step_count = 0
     frame_count = 0
+
     o = env.reset(force_seed=start_seed)
-
     env.vehicle.expert_takeover = True
-
     env.engine.force_fps.disable()
 
     if generate_video:
@@ -180,14 +130,17 @@ if __name__ == '__main__':
                 video_name_interface = video_base_name + "_interface.mp4"
                 # video_list_interface = []
 
-                video_bev = VideoRecorder(video_name=video_name_bev, height=3000, width=3000, code="avc1")
-                video_interface = VideoRecorder(video_name=video_name_interface, height=1200, width=1600, code="avc1")
-
             if ep_count >= environment_num:
                 break
 
+            if generate_video:
+                video_bev = VideoRecorder(video_name=video_name_bev, height=3000, width=3000, code="avc1")
+                video_interface = VideoRecorder(video_name=video_name_interface, height=1200, width=1600, code="avc1")
+
             o = env.reset(force_seed=ep_count + start_seed)
             env.vehicle.expert_takeover = True
+            env.engine.force_fps.disable()
+
 
     # if generate_video:
     # from metadrive.utils.image_to_video import image_list_to_video
