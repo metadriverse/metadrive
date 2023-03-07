7493ac41388f5137e290297bf550434aebaa82a6
QuanyiLi <quanyili0057@gmail.com>
2023-03-07 14:55:12 +0000

    fix rendering issue

--- metadrive/component/lane/abs_lane.py
+++ metadrive/component/lane/abs_lane.py
@@ -308,11 +308,10 @@ class AbstractLane:
             # For visualization
             lane_line = block.loader.loadModel(AssetLoader.file_path("models", "box.bam"))
             lane_line.setScale(length, DrivableAreaProperty.LANE_LINE_WIDTH, DrivableAreaProperty.LANE_LINE_THICKNESS)
-            height = -DrivableAreaProperty.LANE_LINE_GHOST_HEIGHT / 2
+            height = 0
             height += 0.01 if line_color == LineColor.YELLOW else 0
-            lane_line.setPos(Vec3(0, 0, height))
             lane_line.setQuat(LQuaternionf(math.cos(theta / 2), 0, 0, math.sin(theta / 2)))
-            lane_line.setPos(panda_position(middle, DrivableAreaProperty.LANE_LINE_GHOST_HEIGHT / 2))
+            lane_line.setPos(panda_position(middle, height))
             lane_line.reparentTo(parent_np)
             body_np.set_color(line_color)
 
