7382231e7911395a2fbdab33a4f5f86a42c05b44
QuanyiLi <quanyili0057@gmail.com>
2023-03-15 21:22:17 +0000

    try github test image

---
+++ metadrive/tests/test_component/test_offscreen_rendering.py
@@ -0,0 +1,5 @@
+from metadrive.tests.test_installation import verify_installation
+
+
+def test_offscreen_rendering():
+    verify_installation()
--- metadrive/tests/test_installation.py
+++ metadrive/tests/test_installation.py
@@ -24,31 +24,32 @@ def capture_headless_image(image_source="main_camera"):
             },
         )
     )
-    env.reset()
-    for i in range(10):
-        o, r, d, i = env.step([0, 1])
-    assert isinstance(o, dict)
-    print("The observation is a dict with numpy arrays as values: ", {k: v.shape for k, v in o.items()})
-    o = o["image"][..., -1] * 255
-    cv2.imwrite(os.path.join(MetaDrive_PACKAGE_DIR, "examples", "{}_from_observation.png".format(image_source)), o)
-    cam = env.vehicle.image_sensors[image_source]
-    cam.save_image(
-        env.vehicle, os.path.join(MetaDrive_PACKAGE_DIR, "examples", "{}_from_buffer.png".format(image_source))
-    )
-    # if image_source == "main_camera":
-    #     ret = PNMImage()
-    #     env.engine.win.getDisplayRegion(6).camera.node().getDisplayRegion(0).getScreenshot(ret)
-    #     ret.write("test_1.png")
-    #     new_ret = PNMImage()
-    #     RGBCamera._singleton.buffer.getDisplayRegion(1).getScreenshot(new_ret)
-    #     new_ret.write("test_2.png")
-
-    env.close()
-    print(
-        "{} Test result: \nHeadless mode Offscreen render launched successfully! \n"
-        "images named \'{}_from_observation.png\' and \'{}_from_buffer.png\' are saved. "
-        "Open it to check if offscreen mode works well".format(image_source, image_source, image_source)
-    )
+    try:
+        env.reset()
+        for i in range(10):
+            o, r, d, i = env.step([0, 1])
+        assert isinstance(o, dict)
+        print("The observation is a dict with numpy arrays as values: ", {k: v.shape for k, v in o.items()})
+        o = o["image"][..., -1] * 255
+        cv2.imwrite(os.path.join(MetaDrive_PACKAGE_DIR, "examples", "{}_from_observation.png".format(image_source)), o)
+        cam = env.vehicle.image_sensors[image_source]
+        cam.save_image(
+            env.vehicle, os.path.join(MetaDrive_PACKAGE_DIR, "examples", "{}_from_buffer.png".format(image_source))
+        )
+        # if image_source == "main_camera":
+        #     ret = PNMImage()
+        #     env.engine.win.getDisplayRegion(6).camera.node().getDisplayRegion(0).getScreenshot(ret)
+        #     ret.write("test_1.png")
+        #     new_ret = PNMImage()
+        #     RGBCamera._singleton.buffer.getDisplayRegion(1).getScreenshot(new_ret)
+        #     new_ret.write("test_2.png")
+        print(
+            "{} Test result: \nHeadless mode Offscreen render launched successfully! \n"
+            "images named \'{}_from_observation.png\' and \'{}_from_buffer.png\' are saved. "
+            "Open it to check if offscreen mode works well".format(image_source, image_source, image_source)
+        )
+    finally:
+        env.close()
 
 
 def verify_installation():
@@ -57,13 +58,13 @@ def verify_installation():
         env.reset()
         for i in range(1, 100):
             o, r, d, info = env.step([0, 1])
-        env.close()
-        del env
     except:
         print("Error happens in Bullet physics world !")
         sys.exit()
     else:
         print("Bullet physics world is launched successfully!")
+    finally:
+        env.close()
     capture_headless_image()
     capture_headless_image(image_source="rgb_camera")
     capture_headless_image(image_source="depth_camera")
