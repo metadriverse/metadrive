6f7c6ade0c93a9bdf8810c78e8350a6e478ea8ec
QuanyiLi <quanyili0057@gmail.com>
2023-03-23 21:51:14 +0000

    restore waymo type for compatibility

--- metadrive/utils/waymo_utils/waymo_type.py
+++ metadrive/utils/waymo_utils/waymo_type.py
@@ -1,17 +1,7 @@
+from enum import Enum
+
 LaneType = {0: 'UNKNOWN', 1: 'LANE_FREEWAY', 2: 'LANE_SURFACE_STREET', 3: 'LANE_BIKE_LANE'}
-RoadLineType = {
-    0: 'UNKNOWN',
-    1: 'ROAD_LINE_BROKEN_SINGLE_WHITE',
-    2: 'ROAD_LINE_SOLID_SINGLE_WHITE',
-    3: 'ROAD_LINE_DOUBLE_WHITE',
-    4: 'ROAD_LINE_BROKEN_SINGLE_YELLOW',
-    5: 'ROAD_LINE_BROKEN_DOUBLE_YELLOW',
-    6: 'ROAD_LINE_SOLID_SINGLE_YELLOW',
-    7: 'ROAD_LINE_SOLID_DOUBLE_YELLOW',
-    8: 'ROAD_LINE_PASSING_DOUBLE_YELLOW'
-}
-RoadEdgeType = {0: 'UNKNOWN', 1: 'ROAD_EDGE_BOUNDARY', 2: 'ROAD_EDGE_MEDIAN'}
-AgentType = {0: 'UNSET', 1: 'VEHICLE', 2: 'PEDESTRIAN', 3: 'CYCLIST', 4: 'OTHER'}
+
 TrafficSignal = {
     0: 'LANE_STATE_UNKNOWN',
     1: 'LANE_STATE_ARROW_STOP',
@@ -23,3 +13,81 @@ TrafficSignal = {
     7: 'LANE_STATE_FLASHING_STOP',
     8: 'LANE_STATE_FLASHING_CAUTION'
 }
+
+
+class RoadLineType(Enum):
+    UNKNOWN = 0
+    BROKEN_SINGLE_WHITE = 1
+    SOLID_SINGLE_WHITE = 2
+    SOLID_DOUBLE_WHITE = 3
+    BROKEN_SINGLE_YELLOW = 4
+    BROKEN_DOUBLE_YELLOW = 5
+    SOLID_SINGLE_YELLOW = 6
+    SOLID_DOUBLE_YELLOW = 7
+    PASSING_DOUBLE_YELLOW = 8
+
+    @staticmethod
+    def is_road_line(line):
+        return True if line.__class__ == RoadLineType else False
+
+    @staticmethod
+    def is_yellow(line):
+        return True if line in [
+            RoadLineType.SOLID_DOUBLE_YELLOW, RoadLineType.PASSING_DOUBLE_YELLOW, RoadLineType.SOLID_SINGLE_YELLOW,
+            RoadLineType.BROKEN_DOUBLE_YELLOW, RoadLineType.BROKEN_SINGLE_YELLOW
+        ] else False
+
+    @staticmethod
+    def is_broken(line):
+        return True if line in [
+            RoadLineType.BROKEN_DOUBLE_YELLOW, RoadLineType.BROKEN_SINGLE_YELLOW, RoadLineType.BROKEN_SINGLE_WHITE
+        ] else False
+
+    def __getitem__(self, item):
+        return {
+            RoadLineType.UNKNOWN: 'UNKNOWN',
+            RoadLineType.BROKEN_SINGLE_WHITE: 'ROAD_LINE_BROKEN_SINGLE_WHITE',
+            RoadLineType.SOLID_SINGLE_WHITE: 'ROAD_LINE_SOLID_SINGLE_WHITE',
+            RoadLineType.SOLID_DOUBLE_WHITE: 'ROAD_LINE_DOUBLE_WHITE',
+            RoadLineType.BROKEN_SINGLE_YELLOW: 'ROAD_LINE_BROKEN_SINGLE_YELLOW',
+            RoadLineType.BROKEN_DOUBLE_YELLOW: 'ROAD_LINE_BROKEN_DOUBLE_YELLOW',
+            RoadLineType.SOLID_SINGLE_YELLOW: 'ROAD_LINE_SOLID_SINGLE_YELLOW',
+            RoadLineType.SOLID_DOUBLE_YELLOW: 'ROAD_LINE_SOLID_DOUBLE_YELLOW',
+            RoadLineType.PASSING_DOUBLE_YELLOW: 'ROAD_LINE_PASSING_DOUBLE_YELLOW'
+        }[item]
+
+
+class RoadEdgeType(Enum):
+    UNKNOWN = 0
+    # Physical road boundary that doesn't have traffic on the other side (e.g., a curb or the k-rail on the right side of a freeway).
+    BOUNDARY = 1
+    # Physical road boundary that separates the car from other traffic (e.g. a k-rail or an island).
+    MEDIAN = 2
+
+    @staticmethod
+    def is_road_edge(edge):
+        return True if edge.__class__ == RoadEdgeType else False
+
+    @staticmethod
+    def is_sidewalk(edge):
+        return True if edge == RoadEdgeType.BOUNDARY else False
+
+    def __getitem__(self, item):
+        return {RoadEdgeType.UNKNOWN: 'UNKNOWN',
+                RoadEdgeType.BOUNDARY: 'ROAD_EDGE_BOUNDARY',
+                RoadEdgeType.MEDIAN: 'ROAD_EDGE_MEDIAN'}[item]
+
+
+class AgentType(Enum):
+    UNSET = 0
+    VEHICLE = 1
+    PEDESTRIAN = 2
+    CYCLIST = 3
+    OTHER = 4
+
+    def __getitem__(self, item):
+        return {AgentType.UNSET: 'UNSET',
+                AgentType.VEHICLE: 'VEHICLE',
+                AgentType.PEDESTRIAN: 'PEDESTRIAN',
+                AgentType.CYCLIST: 'CYCLIST',
+                AgentType.OTHER: 'OTHER'}[item]
