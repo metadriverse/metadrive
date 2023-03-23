ddd0d5a5bd8fa8b739cae6378c87a4a11452dd59
Alan <fenglan18@outlook.com>
2023-03-23 12:17:10 +0100

    update waymo data process script

--- metadrive/utils/waymo_utils/waymo_utils.py
+++ metadrive/utils/waymo_utils/waymo_utils.py
@@ -17,65 +17,57 @@ import os
 import pickle
 import numpy as np
 
+LaneType = {
+    0: 'UNKNOWN',
+    1: 'LANE_FREEWAY',
+    2: 'LANE_SURFACE_STREET',
+    3: 'LANE_BIKE_LANE'
+}
 
-class RoadLineType(Enum):
-    UNKNOWN = 0
-    BROKEN_SINGLE_WHITE = 1
-    SOLID_SINGLE_WHITE = 2
-    SOLID_DOUBLE_WHITE = 3
-    BROKEN_SINGLE_YELLOW = 4
-    BROKEN_DOUBLE_YELLOW = 5
-    SOLID_SINGLE_YELLOW = 6
-    SOLID_DOUBLE_YELLOW = 7
-    PASSING_DOUBLE_YELLOW = 8
+RoadLineType = {
+    0: 'UNKNOWN',
+    1: 'ROAD_LINE_BROKEN_SINGLE_WHITE',
+    2: 'ROAD_LINE_SOLID_SINGLE_WHITE',
+    3: 'ROAD_LINE_DOUBLE_WHITE',
+    4: 'ROAD_LINE_BROKEN_SINGLE_YELLOW',
+    5: 'ROAD_LINE_BROKEN_DOUBLE_YELLOW',
+    6: 'ROAD_LINE_SOLID_SINGLE_YELLOW',
+    7: 'ROAD_LINE_SOLID_DOUBLE_YELLOW',
+    8: 'ROAD_LINE_PASSING_DOUBLE_YELLOW'
+}
 
-    @staticmethod
-    def is_road_line(line):
-        return True if line.__class__ == RoadLineType else False
+RoadEdgeType = {
+    0: 'UNKNOWN',
+    1: 'ROAD_EDGE_BOUNDARY',
+    2: 'ROAD_EDGE_MEDIAN'
+}
 
-    @staticmethod
-    def is_yellow(line):
-        return True if line in [
-            RoadLineType.SOLID_DOUBLE_YELLOW, RoadLineType.PASSING_DOUBLE_YELLOW, RoadLineType.SOLID_SINGLE_YELLOW,
-            RoadLineType.BROKEN_DOUBLE_YELLOW, RoadLineType.BROKEN_SINGLE_YELLOW
-        ] else False
+AgentType = {
+    0: 'UNSET',
+    1: 'VEHICLE',
+    2: 'PEDESTRIAN',
+    3: 'CYCLIST',
+    4: 'OTHER'
+}
 
-    @staticmethod
-    def is_broken(line):
-        return True if line in [
-            RoadLineType.BROKEN_DOUBLE_YELLOW, RoadLineType.BROKEN_SINGLE_YELLOW, RoadLineType.BROKEN_SINGLE_WHITE
-        ] else False
-
-
-class RoadEdgeType(Enum):
-    UNKNOWN = 0
-    # Physical road boundary that doesn't have traffic on the other side (e.g., a curb or the k-rail on the right side of a freeway).
-    BOUNDARY = 1
-    # Physical road boundary that separates the car from other traffic (e.g. a k-rail or an island).
-    MEDIAN = 2
-
-    @staticmethod
-    def is_road_edge(edge):
-        return True if edge.__class__ == RoadEdgeType else False
-
-    @staticmethod
-    def is_sidewalk(edge):
-        return True if edge == RoadEdgeType.BOUNDARY else False
-
-
-class AgentType(Enum):
-    UNSET = 0
-    VEHICLE = 1
-    PEDESTRIAN = 2
-    CYCLIST = 3
-    OTHER = 4
+TrafficSignal = {
+    0: 'LANE_STATE_UNKNOWN',
+    1: 'LANE_STATE_ARROW_STOP',
+    2: 'LANE_STATE_ARROW_CAUTION',
+    3: 'LANE_STATE_ARROW_GO',
+    4: 'LANE_STATE_STOP',
+    5: 'LANE_STATE_CAUTION',
+    6: 'LANE_STATE_GO',
+    7: 'LANE_STATE_FLASHING_STOP',
+    8: 'LANE_STATE_FLASHING_CAUTION'
+}
 
 
 def extract_poly(message):
     x = [i.x for i in message]
     y = [i.y for i in message]
     z = [i.z for i in message]
-    coord = np.stack((x, y, z), axis=1)
+    coord = np.stack((x, y, z), axis=1).astype('float32')
     return coord
 
 
@@ -84,9 +76,10 @@ def extract_boundaries(fb):
     # b = np.zeros([len(fb), 4], dtype='int64')
     for k in range(len(fb)):
         c = dict()
-        c['index'] = [fb[k].lane_start_index, fb[k].lane_end_index]
-        c['type'] = RoadLineType(fb[k].boundary_type)
-        c['id'] = fb[k].boundary_feature_id
+        c['lane_start_index'] = fb[k].lane_start_index
+        c['lane_end_index'] = fb[k].lane_end_index
+        c['boundary_type'] = RoadLineType[fb[k].boundary_type]
+        c['boundary_feature_id'] = fb[k].boundary_feature_id
         b.append(c)
 
     return b
@@ -96,15 +89,12 @@ def extract_neighbors(fb):
     nbs = []
     for k in range(len(fb)):
         nb = dict()
-        nb['id'] = fb[k].feature_id
-        nb['indexes'] = [
-            fb[k].self_start_index, fb[k].self_end_index, fb[k].neighbor_start_index, fb[k].neighbor_end_index
-        ]
-        nb['indexes'] = [
-            fb[k].self_start_index, fb[k].self_end_index, fb[k].neighbor_start_index, fb[k].neighbor_end_index
-        ]
+        nb['feature_id'] = fb[k].feature_id
+        nb['self_start_index'] = fb[k].self_start_index
+        nb['self_end_index'] = fb[k].self_end_index
+        nb['neighbor_start_index'] = fb[k].neighbor_start_index
+        nb['neighbor_end_index'] = fb[k].neighbor_end_index
         nb['boundaries'] = extract_boundaries(fb[k].boundaries)
-        nb['id'] = fb[k].feature_id
         nbs.append(nb)
     return nbs
 
@@ -112,16 +102,17 @@ def extract_neighbors(fb):
 def extract_center(f):
     center = dict()
     f = f.lane
+    center['speed_limit_mph'] = f.speed_limit_mph
 
-    center['type'] = 'center_lane'
+    center['type'] = LaneType[f.type]
 
     center['polyline'] = extract_poly(f.polyline)
 
     center['interpolating'] = f.interpolating
 
-    center['entry'] = [x for x in f.entry_lanes]
+    center['entry_lanes'] = [x for x in f.entry_lanes]
 
-    center['exit'] = [x for x in f.exit_lanes]
+    center['exit_lanes'] = [x for x in f.exit_lanes]
 
     center['left_boundaries'] = extract_boundaries(f.left_boundaries)
 
@@ -137,16 +128,16 @@ def extract_center(f):
 def extract_line(f):
     line = dict()
     f = f.road_line
-    line['type'] = RoadLineType(f.type)
+    line['type'] = RoadLineType[f.type]
     line['polyline'] = extract_poly(f.polyline)
     return line
 
 
 def extract_edge(f):
     edge = dict()
-    f = f.road_edge
-    edge['type'] = RoadEdgeType(f.type)
-    edge['polyline'] = extract_poly(f.polyline)
+    f_ = f.road_edge
+    edge['type'] = RoadEdgeType[f_.type]
+    edge['polyline'] = extract_poly(f_.polyline)
 
     return edge
 
@@ -154,25 +145,25 @@ def extract_edge(f):
 def extract_stop(f):
     stop = dict()
     f = f.stop_sign
-    stop['type'] = 'stop_sign'
-    stop['lanes'] = [x for x in f.lane]
-    stop['pos'] = [f.position.x, f.position.y, f.position.z]
+    stop['type'] = 'STOP_SIGN'
+    stop['lane'] = [x for x in f.lane]
+    stop['position'] = np.array([f.position.x, f.position.y, f.position.z], dtype='float32')
     return stop
 
 
 def extract_crosswalk(f):
     cross_walk = dict()
     f = f.crosswalk
-    cross_walk['sign'] = 'cross_walk'
-    cross_walk['polygon'] = extract_poly(f.polygon)
+    cross_walk['type'] = 'CROSS_WALK'
+    cross_walk['polyline'] = extract_poly(f.polygon)
     return cross_walk
 
 
 def extract_bump(f):
     speed_bump_data = dict()
     f = f.speed_bump
-    speed_bump_data['type'] = "speed_bump"
-    speed_bump_data['polygon'] = extract_poly(f.polygon)
+    speed_bump_data['type'] = "SPEED_BUMP"
+    speed_bump_data['polyline'] = extract_poly(f.polygon)
 
     return speed_bump_data
 
@@ -182,59 +173,69 @@ def extract_tracks(f, sdc_idx):
 
     for i in range(len(f)):
         agent = dict()
-        agent['type'] = AgentType(f[i].object_type)
+        agent['type'] = AgentType[f[i].object_type]
         x = [state.center_x for state in f[i].states]
         y = [state.center_y for state in f[i].states]
         z = [state.center_z for state in f[i].states]
+        agent['position'] = np.stack([x, y, z], 1).astype('float32')
         l = [state.length for state in f[i].states]
         w = [state.width for state in f[i].states]
         h = [state.height for state in f[i].states]
-        head = [state.heading for state in f[i].states]
+        agent['size'] = np.stack([l, w, h], 1).astype('float32')
+        heading = [state.heading for state in f[i].states]
+        agent['heading'] = np.array(heading, dtype='float32')[:, np.newaxis]
         vx = [state.velocity_x for state in f[i].states]
         vy = [state.velocity_y for state in f[i].states]
+        agent['velocity'] = np.stack([vx, vy], 1).astype('float32')
         valid = [state.valid for state in f[i].states]
-        agent['state'] = np.stack((x, y, z, l, w, h, head, vx, vy, valid), 1)
+        agent['valid'] = np.array(valid)[:, np.newaxis]
         track[f[i].id] = agent
 
     return track, f[sdc_idx].id
 
 
 def extract_map(f):
-    data_dict = dict()
+    map_features = {}
 
     for i in range(len(f)):
-        if f[i].HasField('lane'):
-            data_dict[f[i].id] = extract_center(f[i])
+        f_i = f[i]
+        id = f_i.id
+        if f_i.HasField('lane'):
+            map_features[id] = extract_center(f_i)
 
-        if f[i].HasField('road_line'):
-            data_dict[f[i].id] = extract_line(f[i])
+        if f_i.HasField('road_line'):
+            map_features[id] = extract_line(f_i)
 
-        if f[i].HasField('road_edge'):
-            data_dict[f[i].id] = extract_edge(f[i])
+        if f_i.HasField('road_edge'):
+            map_features[id] = extract_edge(f_i)
 
-        if f[i].HasField('stop_sign'):
-            data_dict[f[i].id] = extract_stop(f[i])
+        if f_i.HasField('stop_sign'):
+            map_features[id] = extract_stop(f_i)
 
-        if f[i].HasField('crosswalk'):
-            data_dict[f[i].id] = extract_crosswalk(f[i])
+        if f_i.HasField('crosswalk'):
+            map_features[id] = extract_crosswalk(f_i)
 
-        if f[i].HasField('speed_bump'):
-            data_dict[f[i].id] = extract_bump(f[i])
+        if f_i.HasField('speed_bump'):
+            map_features[id] = extract_bump(f_i)
 
-    return data_dict
+    return map_features
 
 
 def extract_dynamic(f):
     dynamics = []
     for i in range(len(f)):
-        states = f[i].lane_states
-        one = dict()
-        for j in range(len(states)):
-            state = dict()
-            state['state'] = states[j].state
-            state['stop_point'] = [states[j].stop_point.x, states[j].stop_point.y, states[j].stop_point.z]
-            one[states[j].lane] = state
-    dynamics.append(one)
+        f_i = f[i].lane_states
+        tls_t = []
+        for j in range(len(f_i)):
+            f_i_j = f_i[j]
+            tls_t_j = dict()
+            tls_t_j['lane'] = f_i_j.lane
+            tls_t_j['state'] = TrafficSignal[f_i_j.state]
+            tls_t_j['stop_point'] = np.array([f_i_j.stop_point.x, f_i_j.stop_point.y, f_i_j.stop_point.z],
+                                             dtype='float32')
+            tls_t.append(tls_t_j)
+
+        dynamics.append(tls_t)
 
     return dynamics
 
@@ -282,17 +283,17 @@ def nearest_point(point, line):
 
 
 def extract_width(map, polyline, boundary):
-    l_width = np.zeros(polyline.shape[0])
+    l_width = np.zeros(polyline.shape[0], dtype='float32')
     for b in boundary:
-        lb = map[b['id']]
+        lb = map[b['boundary_feature_id']]
         b_polyline = lb['polyline'][:, :2]
 
-        start_p = polyline[b['index'][0]]
+        start_p = polyline[b['lane_start_index']]
         start_index = nearest_point(start_p, b_polyline)
-        seg_len = b['index'][1] - b['index'][0]
+        seg_len = b['lane_end_index'] - b['lane_start_index']
         end_index = min(start_index + seg_len, lb['polyline'].shape[0] - 1)
-        length = min(end_index - start_index, b['index'][1] - b['index'][0]) + 1
-        self_range = range(b['index'][0], b['index'][0] + length)
+        length = min(end_index - start_index, b['lane_end_index'] - b['lane_start_index']) + 1
+        self_range = range(b['lane_start_index'], b['lane_start_index'] + length)
         bound_range = range(start_index, start_index + length)
         centerLane = polyline[self_range]
         bound = b_polyline[bound_range]
@@ -303,12 +304,12 @@ def extract_width(map, polyline, boundary):
 
 
 def compute_width(map):
-    for key in map.keys():
-        if not 'type' in map[key] or map[key]['type'] != 'center_lane':
-            continue
-        lane = map[key]
+    for id, lane in map.items():
 
-        width = np.zeros((lane['polyline'].shape[0], 2))
+        if not 'LANE' in lane['type']:
+            continue
+
+        width = np.zeros((lane['polyline'].shape[0], 2), dtype='float32')
 
         width[:, 0] = extract_width(map, lane['polyline'][:, :2], lane['left_boundaries'])
         width[:, 1] = extract_width(map, lane['polyline'][:, :2], lane['right_boundaries'])
@@ -327,21 +328,28 @@ def parse_data(input, output_path):
     file_list = os.listdir(input)
     for file in tqdm(file_list):
         file_path = os.path.join(input, file)
-        if not 'scenario' in file_path:
+        if not 'tfrecord' in file_path:
             continue
         dataset = tf.data.TFRecordDataset(file_path, compression_type='')
         for j, data in enumerate(dataset.as_numpy_iterator()):
             scenario.ParseFromString(data)
             scene = dict()
             scene['id'] = scenario.scenario_id
+
+            scene['version'] = 'Mar23'  # March of 2023
+
             scene['ts'] = [ts for ts in scenario.timestamps_seconds]
+
             scene['tracks'], sdc_id = extract_tracks(scenario.tracks, scenario.sdc_track_index)
+
+            scene['sdc_track_index'] = sdc_id
+
             scene['dynamic_map_states'] = extract_dynamic(scenario.dynamic_map_states)
-            scene['sdc_index'] = sdc_id
-            # scene['interact_tracks'] = [x for x in scenario.objects_of_interest]
-            # scene['motion_tracks'] = [x for x in scenario.tracks_to_predict]
-            scene['map'] = extract_map(scenario.map_features)
-            compute_width(scene['map'])
+
+            scene['map_features'] = extract_map(scenario.map_features)
+
+            compute_width(scene['map_features'])
+
             p = os.path.join(output_path, f'{cnt}.pkl')
             with open(p, 'wb') as f:
                 pickle.dump(scene, f)
@@ -358,7 +366,10 @@ def convert_polyline_to_metadrive(waymo_polyline):
 
 if __name__ == "__main__":
     case_data_path = sys.argv[1]
-    os.mkdir(case_data_path + "_processed")
+    try:
+        os.mkdir(case_data_path + "_processed")
+    except:
+        pass
     raw_data_path = case_data_path
     processed_data_path = case_data_path + "_processed"
     # parse raw data from input path to output path,
