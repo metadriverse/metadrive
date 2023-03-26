56836cbebb3c340a857c20ffd44e5b2248f46c7c
PENG Zhenghao <pzh@cs.ucla.edu>
2023-03-26 12:57:28 -0700

    Improve Waymo Env termination condition; fix set_state bugs (#361)
    * add static of route completion for Waymo env
    
    * remove two prints
    
    * fix waymo stat in single-agent
    
    * fix
    
    * fix
    
    * add fix to done function
    
    * fix bugs in setting attributs to vehicle
    
    * format
    
    * clean code
    
    * fix compatibility issue
    
    * fix compatibility

--- metadrive/utils/coordinates_shift.py
+++ metadrive/utils/coordinates_shift.py
@@ -81,6 +81,10 @@ def waymo_to_metadrive_heading(heading):
     return -wrap_to_pi(heading)
 
 
+# Compatibility
+waymo_2_metadrive_heading = waymo_to_metadrive_heading
+
+
 def nuplan_to_metadrive_vector(vector, nuplan_center=(0, 0)):
     "All vec in nuplan should be centered in (0,0) to avoid numerical explosion"
     vector = np.array(vector)
