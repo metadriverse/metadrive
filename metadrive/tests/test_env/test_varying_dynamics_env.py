2804ce71af489a164e2f1eb74dff237f358b3baa
pengzhenghao <pzh@cs.ucla.edu>
2023-03-30 21:13:45 -0700

    add unit test

---
+++ metadrive/tests/test_env/test_varying_dynamics_env.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+from metadrive.envs import VaryingDynamicsEnv
+
+
+def test_varying_dynamics_env():
+    env = VaryingDynamicsEnv({
+        "environment_num": 10
+    })
+    try:
+        dys = []
+        for seed in range(10):
+            env.reset(force_seed=seed)
+            for _ in range(10):
+                env.step(env.action_space.sample())
+            dy = env.vehicle.get_dynamics_parameters()
+            print("Dynamics: ", dy)
+            dy = np.array(list(dy.values()))
+            if len(dys) > 0:
+                for dyy in dys:
+                    assert not np.all(dy == dyy)
+            dys.append(dy)
+    finally:
+        env.close()
+
+
+if __name__ == "__main__":
+    test_varying_dynamics_env()
