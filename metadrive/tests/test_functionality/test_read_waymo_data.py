ef4b989b3e0a420c912bbc4151e701e934c54dde
Quanyi Li <quanyili0057@gmail.com>
2023-04-24 00:54:14 +0100

    fix warmup bug (#431)
    * fix warmup bug
    
    * add key valid check
    
    * format
    
    * fix np.bool
    
    * use bool instead of np.bool
    
    * no bool()
    
    * fix boolgsgsgsgs!

--- metadrive/tests/test_functionality/test_read_waymo_data.py
+++ metadrive/tests/test_functionality/test_read_waymo_data.py
@@ -6,5 +6,5 @@ def test_read_waymo_data():
     summary_dict, summary_list = read_dataset_summary(AssetLoader.file_path("waymo", return_raw_style=False))
     for p in summary_list:
         data = read_scenario_data(AssetLoader.file_path("waymo", p, return_raw_style=False))
-        data.sanity_check(data, check_self_type=False)
+        data.sanity_check(data, check_self_type=False, valid_check=False)
         print("Finish: ", p)
