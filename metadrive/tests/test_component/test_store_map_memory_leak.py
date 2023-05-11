3095906472fad36409c43af3ecf41b04c7077c71
QuanyiLi <quanyili0057@gmail.com>
2023-05-11 21:13:42 +0100

    leak when one map

--- metadrive/tests/test_component/test_store_map_memory_leak.py
+++ metadrive/tests/test_component/test_store_map_memory_leak.py
@@ -1,9 +1,10 @@
 import os
-from metadrive.engine.engine_utils import initialize_engine
+from metadrive.engine.engine_utils import initialize_engine, close_engine
 from metadrive.manager.pg_map_manager import PGMapManager
 import tqdm
 import psutil
 import logging
+
 logging.basicConfig(level=logging.DEBUG)
 from metadrive.component.map.pg_map import PGMap
 from metadrive import MetaDriveEnv
@@ -12,13 +13,13 @@ from metadrive import MetaDriveEnv
 def process_memory():
     process = psutil.Process(os.getpid())
     mem_info = process.memory_info()
-    return mem_info.rss / 1024 / 1024 # mb
+    return mem_info.rss / 1024 / 1024  # mb
 
 
 def test_pgdrive_env_memory_leak():
     total_num = 200
     num = 1
-    out_loop_num = int(total_num/num)
+    out_loop_num = int(total_num / num)
     env = MetaDriveEnv(dict(store_map=False,
                             num_scenarios=num,
                             traffic_density=0.))
@@ -27,7 +28,7 @@ def test_pgdrive_env_memory_leak():
         for i in range(out_loop_num):
             for i in range(num):
                 obs = env.reset(force_seed=i)
-            end_memory = process_memory()
+        end_memory = process_memory()
         print("Start: {}, End: {}".format(start_memory, end_memory))
     finally:
         env.close()
@@ -36,19 +37,32 @@ def test_pgdrive_env_memory_leak():
 def test_map_destroy():
     default_config = MetaDriveEnv.default_config()
     default_config["num_scenarios"] = 1
+    default_config["store_map"] = False
+
+    total_num = 200
+    num = 1
+    out_loop_num = int(total_num / num)
+    default_config["num_scenarios"] = num
+    default_config["store_map"] = False
+
+    start_memory = process_memory()
     engine = initialize_engine(default_config)
+    try:
+        for i in range(out_loop_num):
+            for i in range(num):
+                engine.seed(i)
+                engine.map_manager = PGMapManager()
+                engine.map_manager.before_reset()
+                engine.map_manager.reset()
+        end_memory = process_memory()
+        print("Start: {}, End: {}".format(start_memory, end_memory))
+    finally:
+        close_engine()
 
-    engine.data_manager = ScenarioDataManager()
-    map = PG(map_index=0)
-    map.attach_to_world()
-    engine.enableMouse()
-    map.road_network.show_bounding_box(engine)
 
-    # argoverse data set is as the same coordinates as panda3d
-    pos = map.get_center_point()
-    engine.main_camera.set_bird_view_pos(pos)
-    while True:
-        map.engine.step()
+    # map = PGMap({'type': 'block_num', 'config': 3, 'lane_width': 3.5, 'lane_num': 3, 'exit_length': 50, 'seed': 0})
+    # map.attach_to_world()
+
 
 if __name__ == '__main__':
     test_pgdrive_env_memory_leak()
