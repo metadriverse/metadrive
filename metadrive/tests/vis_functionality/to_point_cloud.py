90309c4fdbaeb34422615dc395a5596fc3c6bda0
QuanyiLi <quanyili0057@gmail.com>
2023-06-02 18:31:39 +0100

    to pc

--- metadrive/tests/vis_functionality/to_point_cloud.py
+++ metadrive/tests/vis_functionality/to_point_cloud.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     pcds = []
     # for h in range(-180, 180, 20):
     h = 0
-    raw_depth = o3d.io.read_image("D:\\code\\scenarionet\\scenarionet\\tests\\script\\camera_depth.jpg")
+    raw_depth = o3d.io.read_image("C:\\Users\\x1\\Desktop\\neurips_2023\\sensor\\camera_depth.jpg")
     raw_depth = np.array(raw_depth)[..., 0]
     raw_depth *= 255
     raw_depth = raw_depth.astype(np.uint16)
@@ -16,9 +16,15 @@ if __name__ == '__main__':
                           [np.sin(h), np.cos(h), 0, 0],
                           [0, 0, 1, 0],
                           [0, 0, 0, 1]])
+    # extrinsic = np.array([[1, 0, 0, 0],
+    #                       [0, np.cos(h), -np.sin(h), 0],
+    #                       [0, np.sin(h), np.cos(h), 1.5],
+    #                       [0, 0, 0, 1]])
+    intrinsic = o3d.camera.PinholeCameraIntrinsic(
+        o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault)
+    intrinsic.set_intrinsics(1600, 900, 0.866, 0.866, 800, 450)
     pcd = o3d.geometry.PointCloud.create_from_depth_image(depth=raw_depth,
-                                                          intrinsic=o3d.camera.PinholeCameraIntrinsic(
-                                                              o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault)
+                                                          intrinsic=intrinsic
                                                           ,
                                                           extrinsic=extrinsic,
                                                           project_valid_depth_only=True,
