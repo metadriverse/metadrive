1dbcfe24f29b381b1485421bd4dc6aee842d232e
QuanyiLi <quanyili0057@gmail.com>
2023-06-14 23:31:29 +0100

    depth

--- metadrive/tests/vis_functionality/batch_point_cloud.py
+++ metadrive/tests/vis_functionality/batch_point_cloud.py
@@ -14,31 +14,34 @@ if __name__ == '__main__':
     # for h in range(-180, 180, 20):
     h = 0
     dir = "C:\\Users\\x1\\Desktop\\neurips_2023\\sensor_video\\depth"
-    s = sorted(os.listdir("C:\\Users\\x1\\Desktop\\neurips_2023\\sensor_video\\depth"), key=lambda x: int(x[6:-4]))
+    files = os.listdir("C:\\Users\\x1\\Desktop\\neurips_2023\\sensor_video\\depth")
+    if "output.mp4" in files:
+        files.remove("output.mp4")
+    s = sorted(files, key=lambda x: int(x[6:-4]))
     for k, file in enumerate(s):
         raw_depth = o3d.io.read_image(os.path.join(dir, file))
         raw_depth = np.array(raw_depth)[..., 0]
         raw_depth *= 255
         raw_depth = raw_depth.astype(np.uint16)
         raw_depth = o3d.geometry.Image(raw_depth)
-        extrinsic = np.array([[np.cos(h), -np.sin(h), 0, 0],
-                              [np.sin(h), np.cos(h), 0, 0],
-                              [0, 0, 1, 0],
-                              [0, 0, 0, 1]])
-        # extrinsic = np.array([[1, 0, 0, 0],
-        #                       [0, np.cos(h), -np.sin(h), 0],
-        #                       [0, np.sin(h), np.cos(h), 1.5],
+        # extrinsic = np.array([[np.cos(h), -np.sin(h), 0, 0],
+        #                       [np.sin(h), np.cos(h), 0, 0],
+        #                       [0, 0, 1, 0],
         #                       [0, 0, 0, 1]])
+        extrinsic = np.array([[1, 0, 0, 0],
+                              [0, np.cos(h), -np.sin(h), 0],
+                              [0, np.sin(h), np.cos(h), 0],
+                              [0, 0, 0, 1]])
         intrinsic = o3d.camera.PinholeCameraIntrinsic(o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault)
-        intrinsic.set_intrinsics(1600, 900, 0.866, 0.866, 800, 450)
+        intrinsic.set_intrinsics(1600, 900, 1.866, 2.066, 800, 200)
         pcd = o3d.geometry.PointCloud.create_from_depth_image(
             depth=raw_depth,
             intrinsic=intrinsic,
             extrinsic=extrinsic,
-            project_valid_depth_only=False,
-            stride=5
-            # depth_scale=1000.0,
-            # depth_trunc=1000.0
+            project_valid_depth_only=True,
+            stride=5,
+            # depth_scale=10000.0,
+            # depth_trunc=10000
         )
         # pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
         r = pcd.get_rotation_matrix_from_xyz((0, 0, np.pi))
@@ -52,7 +55,7 @@ if __name__ == '__main__':
         # ctr.set_up([0, -1, 0])
         # ctr.set_front([0, 0, -1])
         # ctr.set_lookat([1, 0, 0])
-        ctr.set_zoom(0.3)
+        ctr.set_zoom(0.25)
         #
         # camera_parameters = ctr.convert_to_pinhole_camera_parameters()
         # ex = np.array(camera_parameters.extrinsic)
