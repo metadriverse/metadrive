7336bb25de70ef4836379591105ef4234adf17cb
gpi <gpimpale29@gmail.com>
2023-06-20 10:33:41 -0700

    upgrade wrapper

--- metadrive/envs/base_env.py
+++ metadrive/envs/base_env.py
@@ -359,7 +359,7 @@ class BaseEnv(gym.Env):
             ret = self._render_topdown(text=text, *args, **kwargs)
             return ret
         assert self.config["use_render"] or self.engine.mode != RENDER_MODE_NONE, \
-            ("Panda Renderring is off now, can not render. Please set config['use_render'] = True!")
+            ("Panda Rendering is off now, can not render. Please set config['use_render'] = True!")
 
         self.engine.render_frame(text)
 
--- metadrive/envs/gym_wrapper.py
+++ metadrive/envs/gym_wrapper.py
@@ -1,73 +1,108 @@
 try:
+    import inspect
     from typing import Any, Dict
     import gymnasium
     import gym
     import gym.spaces
 
-    class GymEnvWrapper(gym.Env):
-        def __init__(self, config: Dict[str, Any]):
-            """
-            Note that config must contain two items:
-            "inner_class": the class of a Metadrive environment (not instantiated)
-            "inner_config": The config that will be passed to the Metadrive environment
-            """
-            inner_class = config["inner_class"]
-            inner_config = config["inner_config"]
-            assert isinstance(inner_class, type)
-            assert isinstance(inner_config, dict)
-            self._inner = inner_class(config=inner_config)
+    def gymnasiumToGym(space:gymnasium.spaces.Space) -> gym.spaces.Space:
+        if isinstance(space, gymnasium.spaces.Box):
+            return gym.spaces.Box(low=space.low, high=space.high, shape=space.shape)
+        elif isinstance(space, gymnasium.spaces.Discrete):
+            return gym.spaces.Discrete(n=int(space.n), start=int(space.start))
+        elif isinstance(space, gymnasium.spaces.Tuple):
+            return gym.spaces.Tuple([gymnasiumToGym(subspace) for subspace in space.spaces])
+        elif isinstance(space, gymnasium.spaces.Dict):
+            return gym.spaces.Dict({key: gymnasiumToGym(subspace) for key, subspace in space.spaces.items()})
+        else:
+            raise ValueError("unsupported space")
 
-        def step(self, actions):
-            o, r, tm, tc, i = self._inner.step(actions)
-            if isinstance(tm, dict) and isinstance(tc, dict):
-                d = {tm[j] or tc[j] for j in set(list(tm.keys()) + list(tc.keys()))}
-            else:
-                d = tm or tc
-            return o, r, d, i
+    def defined_in_this_file(a):
+        return inspect.getfile(a) == inspect.getfile(createGymWrapper)
 
-        def reset(self, *, seed=None, options=None):
-            # pass non-none parameters to the reset (which may not support options or seed)
-            params = {"seed": seed, "options": options}
-            not_none_params = {k: v for k, v in params.items() if v is not None}
-            obs, _ = self._inner.reset(**not_none_params)
-            return obs
+    def createGymWrapper(inner_class: type):
+        """
+        "inner_class": A gymnasium based Metadrive environment class
+        """
+        class GymEnvWrapper(gym.Env):
 
-        def render(self, *args, **kwargs):
-            # remove mode from kwargs
-            kwargs.pop("mode", None)
-            return self._inner.render(*args, **kwargs)
+            @classmethod
+            def default_config(cls):
+                """
+                This is the default, if you override it, then we will override it within the inner_class to maintain consistency
+                """
+                return inner_class.default_config()
 
-        def close(self):
-            self._inner.close()
+            def __init__(self, config: Dict[str, Any]):
+                # We can only tell if someone has overriden the default config method at init time.
 
-        def seed(self, seed=None):
-            """
-            We cannot seed a Gymnasium environment while running, so do nothing
-            """
-            pass
+                # we know that the function is overriden if the function was not defined in this file
+                # because this is a dynamic class, equality checks will always return false
+                # TODO: this is a hack, but i'm not sure how to make it more robust
+                if not defined_in_this_file(type(self).default_config):
+                    current_wrapper_default_config = type(self).default_config
+                    # at this point, if there was an override, we need to provide the overriden method to the inner class.
+                    class OverridenDefaultConfigWrapper(inner_class):
+                        @classmethod
+                        def default_config(cls):
+                            print(current_wrapper_default_config)
+                            print(GymEnvWrapper.default_config)
+                            return current_wrapper_default_config()
+                    # init now has access to the new default_config
+                    self._inner = OverridenDefaultConfigWrapper(config=config)
+                else:
+                    # if no override, directly initialize
+                    self._inner = inner_class(config=config)
 
-        @property
-        def observation_space(self):
-            obs_space = self._inner.observation_space
-            assert isinstance(obs_space, gymnasium.spaces.Box)
-            return gym.spaces.Box(low=obs_space.low, high=obs_space.high, shape=obs_space.shape)
+            def step(self, actions):
+                o, r, tm, tc, i = self._inner.step(actions)
+                if isinstance(tm, dict) and isinstance(tc, dict):
+                    d = {j: (j in tm and tm[j]) or (j in tc and tc[j]) for j in set(list(tm.keys()) + list(tc.keys()))}
+                else:
+                    d = tm or tc
+                return o, r, d, i
 
-        @property
-        def action_space(self):
-            action_space = self._inner.action_space
-            assert isinstance(action_space, gymnasium.spaces.Box)
-            return gym.spaces.Box(low=action_space.low, high=action_space.high, shape=action_space.shape)
+            def reset(self, *, seed=None, options=None):
+                # pass non-none parameters to the reset (which may not support options or seed)
+                params = {"seed": seed, "options": options}
+                not_none_params = {k:v for k, v in params.items() if v is not None}
+                obs, _ = self._inner.reset(**not_none_params)
+                return obs
 
-        def __getattr__(self, __name: str) -> Any:
-            return self._inner[__name]
+            def render(self, *args, **kwargs):
+                # remove mode from kwargs
+                kwargs.pop("mode", None)
+                return self._inner.render(*args, **kwargs)
+
+            def close(self):
+                self._inner.close()
+
+            def seed(self, seed=None):
+                """
+                We cannot seed a Gymnasium environment while running, so do nothing
+                """
+                pass
+
+            @property
+            def observation_space(self):
+                return gymnasiumToGym(self._inner.observation_space)
+
+            @property
+            def action_space(self):
+                return gymnasiumToGym(self._inner.action_space)
+
+            def __getattr__(self, __name: str) -> Any:
+                return getattr(self._inner, __name)
+        return GymEnvWrapper
+
 
     if __name__ == '__main__':
         from metadrive.envs.scenario_env import ScenarioEnv
 
         env = GymEnvWrapper(config={"inner_class": ScenarioEnv, "inner_config": {"manual_control": True}})
         o, i = env.reset()
-        assert isinstance(env.observation_space, gymnasium.Space)
-        assert isinstance(env.action_space, gymnasium.Space)
+        assert isinstance(env.observation_space, gymnasium.spaces.Space)
+        assert isinstance(env.action_space, gymnasium.spaces.Space)
         for s in range(600):
             o, r, d, i = env.step([0, -1])
             env.vehicle.set_velocity([0, 0])
