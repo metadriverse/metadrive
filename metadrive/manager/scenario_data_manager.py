7f5f79543efd450e6c56e01734da879212ae77e9
QuanyiLi <quanyili0057@gmail.com>
2023-08-14 14:48:30 +0100

    format

--- metadrive/manager/scenario_data_manager.py
+++ metadrive/manager/scenario_data_manager.py
@@ -24,10 +24,12 @@ class ScenarioDataManager(BaseManager):
 
         # for multi-worker
         self.worker_index = self.engine.global_config["worker_index"]
-        self.available_scenario_indices = [i for i in
-                                           range(self.start_scenario_index + self.worker_index,
-                                                 self.start_scenario_index + self.num_scenarios,
-                                                 self.engine.global_config["num_workers"])]
+        self.available_scenario_indices = [
+            i for i in range(
+                self.start_scenario_index + self.worker_index, self.start_scenario_index +
+                self.num_scenarios, self.engine.global_config["num_workers"]
+            )
+        ]
         self._scenarios = {}
 
         # Read summary file first:
@@ -187,7 +189,7 @@ class ScenarioDataManager(BaseManager):
     @property
     def current_scenario_difficulty(self):
         return self.scenario_difficulty[self.summary_lookup[self.engine.global_random_seed]
-        ] if self.scenario_difficulty is not None else 0
+                                        ] if self.scenario_difficulty is not None else 0
 
     @property
     def current_scenario_id(self):
