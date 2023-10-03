673a4eb9868de6d437c3bf1d94fb695c2a866759
QuanyiLi <quanyili0057@gmail.com>
2023-10-03 21:56:42 +0100

    Add connectivity

--- metadrive/component/road_network/node_road_network.py
+++ metadrive/component/road_network/node_road_network.py
@@ -271,15 +271,41 @@ class NodeRoadNetwork(BaseRoadNetwork):
         return next(self.bfs_paths(start_road_node, goal), [])
 
     def get_map_features(self, interval=2):
-        from metadrive.type import MetaDriveType
+
+        def find_entry_exit():
+            entries = dict()
+            exits = dict()
+
+            for _from, _to_dict in self.graph.items():
+                for _to, lanes in _to_dict.items():
+                    lanes = ["{}".format(l.index) for l in lanes]
+                    if _from in exits:
+                        exits[_from] += lanes
+                    else:
+                        exits[_from] = lanes
+
+                    if _to in entries:
+                        entries[_to] += lanes
+                    else:
+                        entries[_to] = lanes
+            return entries, exits
+
+        entries, exits = find_entry_exit()
 
         ret = {}
         for _from, _to_dict in self.graph.items():
             for _to, lanes in _to_dict.items():
                 for k, lane in enumerate(lanes):
+                    left_n = ["{}".format(l.index) for l in lanes[:k]]
+                    right_n = ["{}".format(l.index) for l in lanes[k:]]
                     ret["{}".format(lane.index)] = {
                         SD.POLYLINE: lane.get_polyline(interval),
                         SD.POLYGON: lane.polygon,
+                        # Convert to EdgeNetwork
+                        SD.LEFT_NEIGHBORS: left_n,
+                        SD.RIGHT_NEIGHBORS: right_n,
+                        SD.ENTRY: entries[_from],
+                        SD.EXIT: exits[_to],
                         SD.TYPE: lane.metadrive_type,
                         "speed_limit_kmh": lane.speed_limit
                     }
