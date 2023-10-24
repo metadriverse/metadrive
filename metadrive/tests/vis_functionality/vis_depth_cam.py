1c69b0baa217ef6831b38fd80482b11713a46c7e
QuanyiLi <quanyili0057@gmail.com>
2023-10-24 16:36:00 +0100

    ok

--- metadrive/tests/vis_functionality/vis_depth_cam.py
+++ metadrive/tests/vis_functionality/vis_depth_cam.py
@@ -2,18 +2,6 @@ from metadrive.component.sensors.depth_camera import DepthCamera
 from metadrive.envs.safe_metadrive_env import SafeMetaDriveEnv
 
 if __name__ == "__main__":
-
-    def get_image(env):
-        depth_cam = env.vehicle.get_camera(env.vehicle.config["image_source"])
-        rgb_cam = env.vehicle.get_camera("rgb_camera")
-        for h in range(-180, 180, 20):
-            env.engine.graphicsEngine.renderFrame()
-            depth_cam.get_cam().setH(h)
-            rgb_cam.get_cam().setH(h)
-            depth_cam.save_image(env.vehicle, "depth_{}.jpg".format(h))
-            rgb_cam.save_image(env.vehicle, "rgb_{}.jpg".format(h))
-        # env.engine.screenshot()
-
     env = SafeMetaDriveEnv(
         {
             "num_scenarios": 1,
@@ -21,8 +9,8 @@ if __name__ == "__main__":
             "accident_prob": 1.,
             "start_seed": 4,
             "map": "SSSSS",
-            "manual_control": True,
-            "use_render": True,
+            "manual_control": False,
+            "use_render": False,
             "image_observation": True,
             "rgb_clip": True,
             "interface_panel": ["depth_camera"],
@@ -37,11 +25,28 @@ if __name__ == "__main__":
         }
     )
     env.reset()
+
+
+    def get_image(env):
+        depth_cam = env.vehicle.get_camera(env.vehicle.config["image_source"])
+        rgb_cam = env.vehicle.get_camera("rgb_camera")
+        for h in range(-180, 180, 20):
+            env.engine.graphicsEngine.renderFrame()
+            depth_cam.get_cam().setH(h)
+            rgb_cam.get_cam().setH(h)
+            depth_cam.save_image(env.vehicle, "depth_{}.jpg".format(h))
+            rgb_cam.save_image(env.vehicle, "rgb_{}.jpg".format(h))
+        env.engine.screenshot()
+
+
     env.engine.accept("m", get_image, extraArgs=[env])
+    import cv2
 
     for i in range(1, 100000):
         o, r, tm, tc, info = env.step([0, 1])
         assert env.observation_space.contains(o)
+        cv2.imshow('img', o["image"][..., -1])
+        cv2.waitKey(1)
         if env.config["use_render"]:
             # for i in range(ImageObservation.STACK_SIZE):
             #     ObservationType.show_gray_scale_array(o["image"][:, :, i])
