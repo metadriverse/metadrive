968f642a5fd9834b2d0d3835dc41f277357d1a2d
QuanyiLi <quanyili0057@gmail.com>
2024-01-17 19:29:37 +0000

    fix

--- metadrive/tests/test_sensors/test_sensor_creation.py
+++ metadrive/tests/test_sensors/test_sensor_creation.py
@@ -8,6 +8,63 @@ def test_creation():
     """
     Test sensor creation.
     """
+
+    # image_observation: True, use_render: True, request main_camera
+    env = MetaDriveEnv({
+        "show_terrain": False,
+        "window_size": (16, 16),
+        "sensors": {
+            "semantic": (SemanticCamera, 400, 300),
+            "depth": (DepthCamera, 400, 300),
+            "rgb": (RGBCamera, 400, 300),
+            "main_camera": (),
+        },
+        "vehicle_config": dict(image_source="main_camera"),
+        "interface_panel": ["rgb", "main_camera", "dashboard"],
+        "image_observation": True,
+        "use_render": True,
+    })
+    env.reset()
+    assert env.config["sensors"].keys() == {"lidar",
+                                            "side_detector",
+                                            "lane_line_detector",
+                                            "semantic",
+                                            "rgb",
+                                            "main_camera",
+                                            "dashboard",
+                                            "depth"} == env.engine.sensors.keys()
+    assert env.config["interface_panel"] == ["rgb", "dashboard"]
+    assert not env.engine.main_window_disabled
+    env.close()
+
+    # image_observation: True, use_render: False, request main_camera, dashboard
+    env = MetaDriveEnv({
+        "show_terrain": False,
+        "window_size": (16, 16),
+        "sensors": {
+            "semantic": (SemanticCamera, 400, 300),
+            "depth": (DepthCamera, 400, 300),
+            "rgb": (RGBCamera, 400, 300),
+            "main_camera": (),
+        },
+        "vehicle_config": dict(image_source="main_camera"),
+        "interface_panel": ["rgb", "dashboard"],
+        "image_observation": True,
+        "use_render": False,
+    })
+    env.reset()
+    assert env.config["sensors"].keys() == {"lidar",
+                                            "side_detector",
+                                            "lane_line_detector",
+                                            "semantic",
+                                            "rgb",
+                                            "dashboard",
+                                            "main_camera",
+                                            "depth"} == env.engine.sensors.keys()
+    assert env.config["interface_panel"] == ["rgb", "dashboard"]
+    assert not env.engine.main_window_disabled
+    env.close()
+
     # image_observation: False
     env = MetaDriveEnv({
         "show_terrain": False,
@@ -16,6 +73,25 @@ def test_creation():
             "semantic": (SemanticCamera, 400, 300),
             "depth": (DepthCamera, 400, 300),
             "rgb": (RGBCamera, 400, 300),
+        },
+        "interface_panel": ["rgb", "dashboard"],
+        "image_observation": False,  # it is a switch telling metadrive to use rgb as observation
+    })
+    env.reset()
+    assert env.config["sensors"].keys() == {"lidar", "side_detector", "lane_line_detector"} == env.engine.sensors.keys()
+    assert env.config["interface_panel"] == []
+    assert env.engine.main_window_disabled
+    env.close()
+
+    # image_observation: False, request main_camera
+    env = MetaDriveEnv({
+        "show_terrain": False,
+        "window_size": (16, 16),
+        "sensors": {
+            "semantic": (SemanticCamera, 400, 300),
+            "depth": (DepthCamera, 400, 300),
+            "rgb": (RGBCamera, 400, 300),
+            "main_camera": (),
         },
         "interface_panel": ["rgb", "dashboard"],
         "image_observation": False,  # it is a switch telling metadrive to use rgb as observation
