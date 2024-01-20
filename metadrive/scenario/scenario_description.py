386e333b72f48df985bc0c1552e438577193c2e0
pengzhenghao <pzh@cs.ucla.edu>
2024-01-20 11:57:17 -0800

    add docs

--- metadrive/scenario/scenario_description.py
+++ metadrive/scenario/scenario_description.py
@@ -114,6 +114,7 @@ Example:
 import math
 import os
 from collections import defaultdict
+from typing import Optional
 
 import numpy as np
 
@@ -234,9 +235,9 @@ class ScenarioDescription(dict):
             )
             # position heading check
             assert ScenarioDescription.HEADING in obj_state[ScenarioDescription.STATE
-                                                            ], "heading is required for an object"
+            ], "heading is required for an object"
             assert ScenarioDescription.POSITION in obj_state[ScenarioDescription.STATE
-                                                             ], "position is required for an object"
+            ], "position is required for an object"
 
         # Check dynamic_map_state
         assert isinstance(scenario_dict[cls.DYNAMIC_MAP_STATES], dict)
@@ -253,10 +254,11 @@ class ScenarioDescription(dict):
             "You lack these keys in metadata: {}".format(
                 cls.METADATA_KEYS.difference(set(scenario_dict[cls.METADATA].keys()))
             )
-        assert scenario_dict[cls.METADATA][cls.TIMESTEP].shape == (scenario_length, )
+        assert scenario_dict[cls.METADATA][cls.TIMESTEP].shape == (scenario_length,)
 
     @classmethod
     def _check_map_features(cls, map_feature):
+        """Check if all lanes in the map contain the polyline (center line) feature and if they are in correct types."""
         for id, feature in map_feature.items():
             if MetaDriveType.is_lane(feature[ScenarioDescription.TYPE]):
                 assert ScenarioDescription.POLYLINE in feature, "No lane center line in map feature"
@@ -297,7 +299,7 @@ class ScenarioDescription(dict):
             assert state_array.ndim in [1, 2], "Haven't implemented test array with dim {} yet".format(state_array.ndim)
             if state_array.ndim == 2:
                 assert state_array.shape[
-                    1] != 0, "Please convert all state with dim 1 to a 1D array instead of 2D array."
+                           1] != 0, "Please convert all state with dim 1 to a 1D array instead of 2D array."
 
             if state_key == "valid" and valid_check:
                 assert np.sum(state_array) >= 1, "No frame valid for this object. Consider removing it"
@@ -395,6 +397,7 @@ class ScenarioDescription(dict):
 
     @staticmethod
     def _calculate_num_moving_objects(scenario):
+        """Calculate the number of moving objects, whose moving distance > 1m in this scenario."""
         # moving object
         number_summary_dict = {
             ScenarioDescription.SUMMARY.NUM_MOVING_OBJECTS: 0,
@@ -412,13 +415,55 @@ class ScenarioDescription(dict):
         return number_summary_dict
 
     @staticmethod
+    def update_summaries(scenario):
+        """Update the object summary and number summary of one scenario in-place.
+
+        Args:
+            scenario: The input scenario
+
+        Returns:
+            The same scenario with the scenario['metadata']['object/number_summary'] be overwritten.
+        """
+        SD = ScenarioDescription
+
+        # add agents summary
+        summary_dict = {}
+        for track_id, track in scenario[SD.TRACKS].items():
+            summary_dict[track_id] = SD.get_object_summary(object_dict=track, object_id=track_id)
+        scenario[SD.METADATA][SD.SUMMARY.OBJECT_SUMMARY] = summary_dict
+
+        # count some objects occurrence
+        scenario[SD.METADATA][SD.SUMMARY.NUMBER_SUMMARY] = SD.get_number_summary(scenario)
+        return scenario
+
+    @staticmethod
     def get_number_summary(scenario):
+        """Return the stats of all objects in a scenario.
+
+        Examples:
+            {'num_objects': 211,
+             'object_types': {'CYCLIST', 'PEDESTRIAN', 'VEHICLE'},
+             'num_objects_each_type': {'VEHICLE': 184, 'PEDESTRIAN': 25, 'CYCLIST': 2},
+             'num_moving_objects': 69,
+             'num_moving_objects_each_type': defaultdict(int, {'VEHICLE': 52, 'PEDESTRIAN': 15, 'CYCLIST': 2}),
+             'num_traffic_lights': 8,
+             'num_traffic_light_types': {'LANE_STATE_STOP', 'LANE_STATE_UNKNOWN'},
+             'num_traffic_light_each_step': {'LANE_STATE_UNKNOWN': 164, 'LANE_STATE_STOP': 564},
+             'num_map_features': 358,
+             'map_height_diff': 2.4652252197265625}
+
+        Args:
+            scenario: The input scenario.
+
+        Returns:
+            A dict describing the number of different kinds of data.
+        """
         number_summary_dict = {}
 
         # object
         number_summary_dict[ScenarioDescription.SUMMARY.NUM_OBJECTS] = len(scenario[ScenarioDescription.TRACKS])
-        number_summary_dict[ScenarioDescription.SUMMARY.OBJECT_TYPES
-                            ] = set(v["type"] for v in scenario[ScenarioDescription.TRACKS].values())
+        number_summary_dict[ScenarioDescription.SUMMARY.OBJECT_TYPES] = \
+            set(v["type"] for v in scenario[ScenarioDescription.TRACKS].values())
         object_types_counter = defaultdict(int)
         for v in scenario[ScenarioDescription.TRACKS].values():
             object_types_counter[v["type"]] += 1
@@ -443,16 +488,16 @@ class ScenarioDescription(dict):
                 dynamic_object_states_types.add(step_state)
                 dynamic_object_states_counter[step_state] += 1
         number_summary_dict[ScenarioDescription.SUMMARY.NUM_TRAFFIC_LIGHTS
-                            ] = len(scenario[ScenarioDescription.DYNAMIC_MAP_STATES])
+        ] = len(scenario[ScenarioDescription.DYNAMIC_MAP_STATES])
         number_summary_dict[ScenarioDescription.SUMMARY.NUM_TRAFFIC_LIGHT_TYPES] = dynamic_object_states_types
         number_summary_dict[ScenarioDescription.SUMMARY.NUM_TRAFFIC_LIGHTS_EACH_STEP
-                            ] = dict(dynamic_object_states_counter)
+        ] = dict(dynamic_object_states_counter)
 
         # map
         number_summary_dict[ScenarioDescription.SUMMARY.NUM_MAP_FEATURES
-                            ] = len(scenario[ScenarioDescription.MAP_FEATURES])
+        ] = len(scenario[ScenarioDescription.MAP_FEATURES])
         number_summary_dict[ScenarioDescription.SUMMARY.MAP_HEIGHT_DIFF
-                            ] = ScenarioDescription.map_height_diff(scenario[ScenarioDescription.MAP_FEATURES])
+        ] = ScenarioDescription.map_height_diff(scenario[ScenarioDescription.MAP_FEATURES])
         return number_summary_dict
 
     @staticmethod
@@ -480,13 +525,37 @@ class ScenarioDescription(dict):
         return moving_dist
 
     @staticmethod
-    def num_object(scenario, object_type=None):
+    def get_num_objects(scenario, object_type: Optional[str] = None):
+        """Return the number of objects (vehicles, pedestrians, cyclists, ...).
+
+        Args:
+            scenario: The input scenario.
+            object_type: The string of the object type. If None, return the number of all objects.
+
+        Returns:
+            (int) The number of objects.
+        """
         SD = ScenarioDescription
         metadata = scenario[SD.METADATA]
+        if SD.SUMMARY.NUMBER_SUMMARY not in metadata:
+            scenario[SD.METADATA][SD.SUMMARY.NUMBER_SUMMARY] = SD.get_number_summary(scenario)
         if object_type is None:
             return metadata[SD.SUMMARY.NUMBER_SUMMARY][SD.SUMMARY.NUM_OBJECTS]
         else:
             return metadata[SD.SUMMARY.NUMBER_SUMMARY][SD.SUMMARY.NUM_OBJECTS_EACH_TYPE].get(object_type, 0)
+
+    @staticmethod
+    def num_object(scenario, object_type: Optional[str] = None):
+        """Return the number of objects (vehicles, pedestrians, cyclists, ...).
+
+        Args:
+            scenario: The input scenario.
+            object_type: The string of the object type. If None, return the number of all objects.
+
+        Returns:
+            (int) The number of objects.
+        """
+        return ScenarioDescription.get_num_objects(scenario, object_type)
 
     @staticmethod
     def num_moving_object(scenario, object_type=None):
