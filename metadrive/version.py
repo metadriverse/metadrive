36b41e2d6f23277032bb46cfad78fe5fc6492f69
pengzhenghao <pzh@cs.ucla.edu>
2024-01-21 09:46:58 -0800

    raise error if can't find asset version file

--- metadrive/version.py
+++ metadrive/version.py
@@ -6,6 +6,11 @@ VERSION = "0.4.2.1"
 def asset_version():
     asset_path = os.path.join(os.path.dirname(__file__), "assets")
     asset_version = os.path.join(asset_path, "version.txt")
+
+    if not os.path.isfile(asset_version):
+        raise ValueError(
+            "Asset version file version.txt does not exist! Existing files: {}".format(os.listdir(asset_path)))
+
     with open(asset_version, "r") as file:
         lines = file.readlines()
     return lines[0]
