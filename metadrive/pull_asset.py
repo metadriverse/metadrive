b3cfad5c79ea64f9e27ce0f5f1ff9b2702112909
pengzhenghao <pzh@cs.ucla.edu>
2024-01-21 10:34:10 -0800

    optimize the lock in pull_assets

--- metadrive/pull_asset.py
+++ metadrive/pull_asset.py
@@ -35,24 +35,26 @@ class MyProgressBar():
 
 def pull_asset(update):
     logger = get_logger()
+
     assets_folder = ROOT_DIR / "assets"
+    zip_path = ROOT_DIR / 'assets.zip'
+    lock_path = ROOT_DIR / 'assets.lock'
+
+    should_remove_existing_assets = False
     if os.path.exists(assets_folder):
         if not update:
             logger.warning(
-                "Fail to pull. Assets already exists, version: {}. Expected version: {}. "
+                "Fail to update assets. Assets already exists, version: {}. Expected version: {}. "
                 "To overwrite existing assets and update, add flag '--update' and rerun this script".format(
                     asset_version(), VERSION
                 )
             )
             return
         else:
-            logger.info("Remove existing assets, version: {}..".format(asset_version()))
-            shutil.rmtree(assets_folder, ignore_errors=True)
+            should_remove_existing_assets = True
 
-    zip_path = ROOT_DIR / 'assets.zip'
-    zip_lock = ROOT_DIR / 'assets.zip.lock'
-    # filelock
-    lock = filelock.FileLock(zip_lock, timeout=1)
+    lock = filelock.FileLock(lock_path, timeout=1)
+
     # Extract the zip file to the desired location
     try:
         with lock.acquire():
@@ -60,13 +62,18 @@ def pull_asset(update):
             logger.info("Pull assets from {} to {}".format(ASSET_URL, zip_path))
             extra_arg = [MyProgressBar()] if logger.level == logging.INFO else []
             urllib.request.urlretrieve(ASSET_URL, zip_path, *extra_arg)
+
+            if should_remove_existing_assets:
+                logger.info("Remove existing assets, version: {}..".format(asset_version()))
+                shutil.rmtree(assets_folder, ignore_errors=True)
+
             shutil.unpack_archive(filename=zip_path, extract_dir=ROOT_DIR)
-    except Timeout:
+    except Timeout:  # Timeout will be raised if the lock can not be acquired in 1s.
         logger.info(
             "Another instance of this program is already running. "
             "Wait for the asset pulling finished from another program..."
         )
-        while os.path.exists(zip_lock):
+        while os.path.exists(lock_path):
             logger.info("Assets not pulled yet. Waiting for 10 seconds...")
             time.sleep(10)
 
@@ -76,8 +83,8 @@ def pull_asset(update):
         # Remove the downloaded zip file (optional)
         if os.path.exists(zip_path):
             os.remove(zip_path)
-        if os.path.exists(zip_lock):
-            os.remove(zip_lock)
+        if os.path.exists(lock_path):
+            os.remove(lock_path)
 
     # Final check
     if not os.path.exists(assets_folder):
