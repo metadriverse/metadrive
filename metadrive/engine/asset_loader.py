dea5a82e69fbeafd0e96dc346bb94b77e2e9c194
pengzhenghao <pzh@cs.ucla.edu>
2024-01-21 14:41:39 -0800

    remove debug

--- metadrive/engine/asset_loader.py
+++ metadrive/engine/asset_loader.py
@@ -100,11 +100,6 @@ class AssetLoader:
             AssetLoader.file_path("textures", "grass1", "GroundGrassGreen002_COL_1K.jpg", unix_style=False)
         )
 
-        # TODO: DEBUG REMOVE THIS:
-        # print("asset_version_match: ", asset_version_match)
-        # print("grass_texture_exists: ", grass_texture_exists)
-        assert grass_texture_exists, os.listdir(AssetLoader.asset_path)
-
         return (not asset_version_match) or (not grass_texture_exists)
 
 
