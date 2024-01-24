07890a49388977adb995b829da265710e8ce9793
pengzhenghao <pzh@cs.ucla.edu>
2024-01-24 09:17:24 -0800

    some tests

--- metadrive/tests/vis_env/vis_safe_metadrive.py
+++ metadrive/tests/vis_env/vis_safe_metadrive.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
             "manual_control": True,
             "num_scenarios": 1,
             "show_coordinates": True,
-            "map": "SSSS",
+            "map": "CCCC",
             "accident_prob": 1.0,
             "vehicle_config": {
                 "show_lidar": True
@@ -20,6 +20,11 @@ if __name__ == "__main__":
     o, _ = env.reset()
     # print("vehicle num", len(env.engine.traffic_manager.vehicles))
     for i in range(1, 100000):
+        # Print TrafficBarrier pitch and roll.
+        # from metadrive.component.static_object.traffic_object import TrafficBarrier
+        # obj = [v for v in env.engine.object_manager.spawned_objects.values() if isinstance(v, TrafficBarrier)][0]
+        # print(f"{obj.pitch=}, {obj.roll=}, {obj.heading_theta=}, {obj.position=}")
+
         o, r, tm, tc, info = env.step([0, 1])
         env.render(text={})
     env.close()
