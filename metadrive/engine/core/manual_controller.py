9ef4999ab548fe15dd66aff1744d23f7bef234e9
pengzhenghao <pzh@cs.ucla.edu>
2024-01-24 13:10:15 -0800

    revert to old throttle behavior?

--- metadrive/engine/core/manual_controller.py
+++ metadrive/engine/core/manual_controller.py
@@ -107,17 +107,9 @@ class KeyboardController(Controller):
         elif up_key_pressed:
             self.throttle_brake = max(self.throttle_brake, 0.)
             self.throttle_brake += self.THROTTLE_INCREMENT
-            # if self.throttle_brake >= 0.0:  # increment throttle a little.
-            #     self.throttle_brake += self.THROTTLE_INCREMENT
-            # else:
-            #     self.throttle_brake += self.THROTTLE_INCREMENT_WHEN_BRAKE  # increment throttle a little more.
         elif down_key_pressed:
             self.throttle_brake = min(self.throttle_brake, 0.)
             self.throttle_brake -= self.BRAKE_INCREMENT
-            # if self.throttle_brake <= 0.0:
-            #     self.throttle_brake -= self.BRAKE_INCREMENT
-            # else:
-            #     self.throttle_brake -= self.BRAKE_INCREMENT_WHEN_THROTTLE
 
         rand = self.np_random.rand() / 10000
         self.steering += rand
