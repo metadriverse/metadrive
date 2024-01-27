7db3ccf4e6d59b3df4bab382adb4b1d24e35587e
QuanyiLi <quanyili0057@gmail.com>
2024-01-27 16:39:48 +0000

    format

--- metadrive/component/pgblock/pg_block.py
+++ metadrive/component/pgblock/pg_block.py
@@ -22,7 +22,6 @@ class PGBlockSocket:
     Positive_road is right road, and Negative road is left road on which cars drive in reverse direction
     BlockSocket is a part of block used to connect other blocks
     """
-
     def __init__(self, positive_road: Road, negative_road: Road = None):
         self.positive_road = positive_road
         self.negative_road = negative_road if negative_road else None
@@ -75,17 +74,16 @@ class PGBlock(BaseBlock):
     When single-direction block created, road_2 in block socket is useless.
     But it's helpful when a town is created.
     """
-
     def __init__(
-            self,
-            block_index: int,
-            pre_block_socket: PGBlockSocket,
-            global_network: NodeRoadNetwork,
-            random_seed,
-            ignore_intersection_checking=False,
-            remove_negative_lanes=False,
-            side_lane_line_type=None,
-            center_line_type=None,
+        self,
+        block_index: int,
+        pre_block_socket: PGBlockSocket,
+        global_network: NodeRoadNetwork,
+        random_seed,
+        ignore_intersection_checking=False,
+        remove_negative_lanes=False,
+        side_lane_line_type=None,
+        center_line_type=None,
     ):
 
         # Specify the lane line type
