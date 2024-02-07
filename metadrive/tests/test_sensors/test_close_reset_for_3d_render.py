c696a9ba9417b19bdaa0f2c30dba6054503715db
QuanyiLi <quanyili0057@gmail.com>
2024-02-07 11:46:44 +0000

    format.sh

--- metadrive/tests/test_sensors/test_close_reset_for_3d_render.py
+++ metadrive/tests/test_sensors/test_close_reset_for_3d_render.py
@@ -15,13 +15,17 @@ def _test_close_reset_for_3d_render():
         # sensors = {"rgb_camera": (RGBCamera, 1920, 1080)}
         # env = MetaDriveEnv({"image_observation": True, "use_render": False, "force_destroy": True, "preload_models": False, "sensors": sensors, "log_level": logging.CRITICAL})
         ## onscreen
-        env = MetaDriveEnv({"use_render": True,
-                            "traffic_density": 0.0,
-                            "show_skybox": False,
-                            # "show_terrain": False,
-                            "show_sidewalk": False,
-                            # "show_terrain": False,
-                            "log_level": logging.CRITICAL})
+        env = MetaDriveEnv(
+            {
+                "use_render": True,
+                "traffic_density": 0.0,
+                "show_skybox": False,
+                # "show_terrain": False,
+                "show_sidewalk": False,
+                # "show_terrain": False,
+                "log_level": logging.CRITICAL
+            }
+        )
         obs, _ = env.reset()
         # for _ in range(1000000):
         #     env.step(env.action_space.sample())
@@ -30,7 +34,7 @@ def _test_close_reset_for_3d_render():
         del env
 
         mem = psutil.Process().memory_info().rss
-        print(f"Memory usage ({i}): ", mem / (1024 ** 2), "MB")
+        print(f"Memory usage ({i}): ", mem / (1024**2), "MB")
 
 
 if __name__ == '__main__':
