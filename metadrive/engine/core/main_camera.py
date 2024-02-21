0c36c490ce1cfdb641f55f1d417fbd48a802323b
QuanyiLi <quanyili0057@gmail.com>
2024-02-21 17:37:54 +0000

    fix one test

--- metadrive/engine/core/main_camera.py
+++ metadrive/engine/core/main_camera.py
@@ -447,7 +447,7 @@ class MainCamera(BaseSensor):
     def mouse_into_window(self):
         return True if not self._last_frame_has_mouse and self.has_mouse else False
 
-    def perceive(self, clip=True, new_parent_node: Union[NodePath, None] = None, position=None, hpr=None) -> np.ndarray:
+    def perceive(self, to_float=True, new_parent_node: Union[NodePath, None] = None, position=None, hpr=None) -> np.ndarray:
         """
         When clip is set to False, the image will be represented by unit8 with component value ranging from [0-255].
         Otherwise, it will be float type with component value ranging from [0.-1.]. By default, the reset parameters are
@@ -503,7 +503,7 @@ class MainCamera(BaseSensor):
             self.camera.setHpr(original_hpr)
             self.camera.setPos(original_position)
 
-        if not clip:
+        if not to_float:
             return img.astype(np.uint8, copy=False, order="C")
         else:
             return img / 255
