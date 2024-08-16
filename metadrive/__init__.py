12b7c1c44efb96fc894e0075891f4c430470277e
Maxime Desroches <desroches.maxime@gmail.com>
2024-08-16 15:58:38 -0700

    dont force pygame

--- metadrive/__init__.py
+++ metadrive/__init__.py
@@ -1,12 +1,6 @@
-from os import environ
-
-environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
-import pygame  # it is important to import pygame after that
-
-from metadrive.envs import MetaDriveEnv, TopDownMetaDrive, TopDownSingleFrameMetaDriveEnv, TopDownMetaDriveEnvV2, \
-    SafeMetaDriveEnv, MultiAgentRoundaboutEnv, MultiAgentIntersectionEnv, MultiAgentParkingLotEnv, \
-    MultiAgentTollgateEnv, MultiAgentBottleneckEnv, MultiAgentMetaDrive, ScenarioEnv
-from metadrive.utils.registry import get_metadrive_class
 import os
+
+from metadrive.envs import MetaDriveEnv, SafeMetaDriveEnv, ScenarioEnv
+from metadrive.utils.registry import get_metadrive_class
 
 MetaDrive_PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
