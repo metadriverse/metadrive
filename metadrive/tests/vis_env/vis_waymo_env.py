2e386d58474f9174f927629a89da4b4a1aa764bb
QuanyiLi <quanyili0057@gmail.com>
2024-09-22 16:12:54 +0100

    turn off render

--- metadrive/tests/vis_env/vis_waymo_env.py
+++ metadrive/tests/vis_env/vis_waymo_env.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
             "show_policy_mark": render,
             # "agent_policy": ReplayEgoCarPolicy,
             "reactive_traffic": True,
-            # "use_render": render,
+            "use_render": render,
             "data_directory": AssetLoader.file_path(asset_path, "waymo", unix_style=False),
             "num_scenarios": 1,
             "start_scenario_index": 1,
