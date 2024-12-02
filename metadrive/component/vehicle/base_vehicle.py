541cfbb8e70e6a8d5eba8c83bc87539fce201c85
pengzhenghao <pzh@cs.ucla.edu>
2024-12-02 15:24:01 -0800

    Create new car model with different scale and shape, if self.config['scale'] is set.

--- metadrive/component/vehicle/base_vehicle.py
+++ metadrive/component/vehicle/base_vehicle.py
@@ -635,18 +635,34 @@ class BaseVehicle(BaseObject, BaseVehicleState):
 
     def _add_visualization(self):
         if self.render:
-            [path, scale, offset, HPR] = self.path
-            if path not in BaseVehicle.model_collection:
+            path, scale, offset, HPR = self.path
+            should_update = (path not in BaseVehicle.model_collection) or (self.config["scale"] is not None)
+
+            if should_update:
                 car_model = self.loader.loadModel(AssetLoader.file_path("models", path))
                 car_model.setTwoSided(False)
-                BaseVehicle.model_collection[path] = car_model
+                extra_offset_z = -self.TIRE_RADIUS - self.CHASSIS_TO_WHEEL_AXIS
+                if self.config['scale'] is not None:
+                    offset = (
+                        offset[0] * self.config['scale'][0], offset[1] * self.config['scale'][1],
+                        offset[2] * self.config['scale'][2]
+                    )
+                    scale = (self.config['scale'][0], self.config['scale'][1], self.config['scale'][2])
+
+                    # A quick workaround here.
+                    # The model position is set to height/2 in ScenarioMapManager.
+                    # Now we set this offset to -height/2, so that the model will be placed on the ground.
+                    extra_offset_z = -self.config["height"] / 2
+
                 car_model.setScale(scale)
                 # model default, face to y
                 car_model.setHpr(*HPR)
-                car_model.setPos(offset[0], offset[1], offset[-1])
-                car_model.setZ(-self.TIRE_RADIUS - self.CHASSIS_TO_WHEEL_AXIS + offset[-1])
+                car_model.setPos(offset[0], offset[1], offset[2] + extra_offset_z)
+                BaseVehicle.model_collection[path] = car_model
+
             else:
                 car_model = BaseVehicle.model_collection[path]
+
             car_model.instanceTo(self.origin)
             if self.config["random_color"]:
                 material = Material()
