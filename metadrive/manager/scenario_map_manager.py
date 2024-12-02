0e908244958b3542de62212db2a064e273336e49
pengzhenghao <pzh@cs.ucla.edu>
2024-12-02 12:32:07 -0800

    non-breaking changes: Update scenario_map_manager.py, add w/l/h to vehicle config (traffic agent)

--- metadrive/manager/scenario_map_manager.py
+++ metadrive/manager/scenario_map_manager.py
@@ -54,8 +54,13 @@ class ScenarioMapManager(BaseManager):
         sdc_traj = parse_full_trajectory(sdc_track)
 
         init_state = parse_object_state(sdc_track, 0, check_last_state=False)
+        init_state = parse_object_state(sdc_track, 0, check_last_state=False, include_z_position=True)
         last_state = parse_object_state(sdc_track, -1, check_last_state=True)
         init_position = init_state["position"]
+
+        # Add a fake Z axis so that the object will not fall from the sky.
+        init_position[-1] = 0
+
         init_yaw = init_state["heading"]
         last_position = last_state["position"]
         last_yaw = last_state["heading"]
@@ -69,7 +74,11 @@ class ScenarioMapManager(BaseManager):
                 dict(
                     agent_configs={
                         DEFAULT_AGENT: dict(
-                            spawn_position_heading=(init_position, init_yaw), spawn_velocity=init_state["velocity"]
+                            spawn_position_heading=(list(init_position), init_yaw),
+                            spawn_velocity=init_state["velocity"],
+                            width=init_state["width"],
+                            length=init_state["length"],
+                            height=init_state["height"],
                         )
                     }
                 )
