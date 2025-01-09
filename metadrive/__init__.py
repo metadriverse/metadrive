009de70c2ca7089e87c50d15d91ea8c6f757e173
pengzhenghao <pzh@cs.ucla.edu>
2025-01-09 14:18:48 -0800

    fix test

--- metadrive/__init__.py
+++ metadrive/__init__.py
@@ -1,6 +1,10 @@
 import os
 
-from metadrive.envs import MetaDriveEnv, SafeMetaDriveEnv, ScenarioEnv
+from metadrive.envs import (
+    MetaDriveEnv, SafeMetaDriveEnv, ScenarioEnv, BaseEnv, MultiAgentMetaDrive, MultiAgentTollgateEnv,
+    MultiAgentBottleneckEnv, MultiAgentIntersectionEnv, MultiAgentRoundaboutEnv, MultiAgentParkingLotEnv,
+    MultiAgentTinyInter, VaryingDynamicsEnv
+)
 from metadrive.utils.registry import get_metadrive_class
 
 MetaDrive_PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
--- metadrive/envs/__init__.py
+++ metadrive/envs/__init__.py
@@ -3,3 +3,7 @@ from metadrive.envs.safe_metadrive_env import SafeMetaDriveEnv
 from metadrive.envs.varying_dynamics_env import VaryingDynamicsEnv
 from metadrive.envs.scenario_env import ScenarioEnv
 from metadrive.envs.base_env import BaseEnv
+from metadrive.envs.marl_envs import (
+    MultiAgentMetaDrive, MultiAgentTollgateEnv, MultiAgentBottleneckEnv, MultiAgentIntersectionEnv,
+    MultiAgentRoundaboutEnv, MultiAgentParkingLotEnv, MultiAgentTinyInter
+)
