f1b3e7a0b204ea7456ed100795bdee9809130b98
QuanyiLi <quanyili0057@gmail.com>
2025-01-21 23:14:37 +0000

    add test for checking lidar

--- metadrive/tests/test_component/test_traffic_light.py
+++ metadrive/tests/test_component/test_traffic_light.py
@@ -1,9 +1,8 @@
-from metadrive.component.traffic_participants.pedestrian import Pedestrian
 from metadrive.component.traffic_light.base_traffic_light import BaseTrafficLight
 from metadrive.envs.metadrive_env import MetaDriveEnv
 
 
-def test_traffic_light(render=False, manual_control=False, debug=False):
+def test_traffic_light_state_check(render=False, manual_control=False, debug=False):
     env = MetaDriveEnv(
         {
             "num_scenarios": 1,
@@ -27,16 +26,17 @@ def test_traffic_light(render=False, manual_control=False, debug=False):
         env.reset()
         light = env.engine.spawn_object(BaseTrafficLight, lane=env.current_map.road_network.graph[">>>"]["1X1_0_"][0])
         light.set_green()
-        test_success = False
+        test_success = True
         for s in range(1, 100):
             env.step([0, 1])
-            if env.agent.green_light:
-                test_success = True
+            if env.agent.red_light or env.agent.yellow_light:
+                test_success = False
                 break
         assert test_success
         light.destroy()
 
         # red test
+        env.reset()
         light = env.engine.spawn_object(BaseTrafficLight, lane=env.current_map.road_network.graph[">>>"]["1X1_0_"][0])
         light.set_red()
         test_success = False
@@ -47,6 +47,7 @@ def test_traffic_light(render=False, manual_control=False, debug=False):
                 break
         assert test_success
         light.destroy()
+
         # yellow
         env.reset()
         light = env.engine.spawn_object(BaseTrafficLight, lane=env.current_map.road_network.graph[">>>"]["1X1_0_"][0])
@@ -64,5 +65,72 @@ def test_traffic_light(render=False, manual_control=False, debug=False):
         env.close()
 
 
+def test_traffic_light_detection(render=False, manual_control=False, debug=False):
+    env = MetaDriveEnv(
+        {
+            "num_scenarios": 1,
+            "traffic_density": 0.,
+            "traffic_mode": "hybrid",
+            "manual_control": manual_control,
+            "use_render": render,
+            "debug": debug,
+            "debug_static_world": debug,
+            "map": "X",
+            "window_size": (1200, 800),
+            "vehicle_config": {
+                "enable_reverse": True,
+                "show_dest_mark": True
+            },
+        }
+    )
+    env.reset()
+    try:
+        # green
+        env.reset()
+        light = env.engine.spawn_object(BaseTrafficLight, lane=env.current_map.road_network.graph[">>>"]["1X1_0_"][0])
+        light.set_green()
+        test_success = True
+        for s in range(1, 100):
+            env.step([0, 1])
+            if min(env.observations["default_agent"].cloud_points) < 0.99:
+                test_success = False
+                break
+            assert len(env.observations["default_agent"].detected_objects) == 0
+        assert test_success
+        light.destroy()
+
+        # red test
+        env.reset()
+        light = env.engine.spawn_object(BaseTrafficLight, lane=env.current_map.road_network.graph[">>>"]["1X1_0_"][0])
+        light.set_red()
+        test_success = False
+        for s in range(1, 100):
+            env.step([0, 1])
+            if min(env.observations["default_agent"].cloud_points) < 0.5:
+                test_success = True
+                assert list(env.observations["default_agent"].detected_objects)[0].status == BaseTrafficLight.LIGHT_RED
+                break
+        assert test_success
+        light.destroy()
+
+        # yellow
+        env.reset()
+        light = env.engine.spawn_object(BaseTrafficLight, lane=env.current_map.road_network.graph[">>>"]["1X1_0_"][0])
+        light.set_yellow()
+        test_success = False
+        for s in range(1, 100):
+            env.step([0, 1])
+            if min(env.observations["default_agent"].cloud_points) < 0.5:
+                test_success = True
+                assert list(env.observations["default_agent"].detected_objects)[0].status == BaseTrafficLight.LIGHT_YELLOW
+                break
+        assert test_success
+        light.destroy()
+
+    finally:
+        env.close()
+
+
 if __name__ == "__main__":
-    test_traffic_light(True, manual_control=True)
+    # test_traffic_light_state_check(True, manual_control=False)
+    test_traffic_light_detection(True, manual_control=False)
