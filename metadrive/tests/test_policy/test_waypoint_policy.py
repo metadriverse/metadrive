5f35f490fd17fffac6d0c1a3a695332d5b51259a
pengzhenghao <pzh@cs.ucla.edu>
2025-04-12 16:50:34 -0700

    Reformat the waypoint policy unit test, and IT DOESN'T TEST THE CLOSED-LOOP YET!

--- metadrive/tests/test_policy/test_waypoint_policy.py
+++ metadrive/tests/test_policy/test_waypoint_policy.py
@@ -1,47 +1,28 @@
 #!/usr/bin/env python
-import argparse
 
-from metadrive.component.sensors.depth_camera import DepthCamera
-from metadrive.component.sensors.rgb_camera import RGBCamera
-from metadrive.component.sensors.semantic_camera import SemanticCamera
+import numpy as np
+
 from metadrive.constants import HELP_MESSAGE
 from metadrive.engine.asset_loader import AssetLoader
-from metadrive.policy.replay_policy import WayPointPolicy
-import numpy as np
 from metadrive.envs.scenario_env import ScenarioEnv
+from metadrive.policy.replay_policy import WayPointPolicy
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--reactive_traffic", action="store_true")
-    parser.add_argument("--top_down", "--topdown", action="store_true")
-    parser.add_argument("--waymo", action="store_true")
-    parser.add_argument("--add_sensor", action="store_true")
-    args = parser.parse_args()
-    extra_args = dict(film_size=(2000, 2000)) if args.top_down else {}
+
+def test_waypoint_policy():
+    """
+    Test the waypoint policy by running a scenario in the ScenarioEnv.
+    """
     asset_path = AssetLoader.asset_path
-    use_waymo = args.waymo
     print(HELP_MESSAGE)
-
     cfg = {
         "agent_policy": WayPointPolicy,
         "map_region_size": 1024,  # use a large number if your map is toooooo big
         "sequential_seed": True,
-        "reactive_traffic": True if args.reactive_traffic else False,
-        "use_render": False,  #True if not args.top_down else False,
-        "data_directory": AssetLoader.file_path(asset_path, "waymo" if use_waymo else "nuscenes", unix_style=False),
-        "num_scenarios": 3 if use_waymo else 10,
+        "reactive_traffic": False,
+        "use_render": False,  # True if not args.top_down else False,
+        "data_directory": AssetLoader.file_path(asset_path, "nuscenes", unix_style=False),
+        "num_scenarios": 10,
     }
-    if args.add_sensor:
-        additional_cfg = {
-            "interface_panel": ["rgb_camera", "depth_camera", "semantic"],
-            "sensors": {
-                "rgb_camera": (DepthCamera, 256, 256),
-                "depth_camera": (RGBCamera, 256, 256),
-                "semantic": (SemanticCamera, 256, 256)
-            }
-        }
-        cfg.update(additional_cfg)
-
     env = ScenarioEnv(cfg)
     o, _ = env.reset()
     seen_seeds = set()
@@ -51,15 +32,18 @@ if __name__ == "__main__":
     ego_id = scenario["metadata"]["sdc_id"]
     ego_track = scenario["tracks"][ego_id]
     ego_traj = ego_track["state"]["position"][..., :2]
-    ego_traj_world = ego_traj.tolist()
     ego_traj_ego = np.array([env.agent.convert_to_local_coordinates(point, env.agent.position) for point in ego_traj])
     ADEs, FDEs, flag = [], [], True
     for i in range(1, 100000):
+
+        # FIXME: This part looks wierd as we don't set the action except for the first step.
+        #  Then this won't test our waypoint policy in a closed-loop manner.
         if flag:
             action = dict(position=ego_traj_ego)
             flag = False
         else:
             action = None
+
         o, r, tm, tc, info = env.step(actions=action)
         if tm or tc:
             policy = env.engine.get_policy(env.agent.id)
@@ -81,12 +65,17 @@ if __name__ == "__main__":
             ego_id = scenario["metadata"]["sdc_id"]
             ego_track = scenario["tracks"][ego_id]
             ego_traj = ego_track["state"]["position"][..., :2]
-            ego_traj_world = ego_traj.tolist()
             ego_traj_ego = np.array(
                 [env.agent.convert_to_local_coordinates(point, env.agent.position) for point in ego_traj]
             )
+    print(f"Mean ADE: {np.mean(ADEs)}, Mean FDE: {np.mean(FDEs)}")
+
     mean_ade = np.mean(ADEs)
     mean_fde = np.mean(FDEs)
     assert mean_ade < 1 and mean_fde < 2
 
     env.close()
+
+
+if __name__ == '__main__':
+    test_waypoint_policy()
